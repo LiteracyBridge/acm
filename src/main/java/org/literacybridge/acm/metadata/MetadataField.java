28b502ea3c54ea57d22600881b3bfd4123db459f
michibusch <michibusch@359e7cc3-0860-1fdf-adbe-24c20723d3c1>
2010-02-19 05:31:39 +0000

    Improving metadata handling and adding initial device recognition code

--- src/main/java/org/literacybridge/acm/metadata/MetadataField.java
+++ src/main/java/org/literacybridge/acm/metadata/MetadataField.java
@@ -1,9 +1,17 @@
 package org.literacybridge.acm.metadata;
 
 public class MetadataField<T> {
-	MetadataField(Attribute<?>... attributes) {}
+	private Attribute<?>[] attributes;
+	
+	MetadataField(Attribute<?>... attributes) {
+		this.attributes = attributes;
+	}
 	
 	boolean validateValue(T value) {
 		return true;
+	}
+	
+	public Attribute<?>[] getAttributes() {
+		return this.attributes;
 	}
 }
