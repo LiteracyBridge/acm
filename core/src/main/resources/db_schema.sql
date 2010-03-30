629387164c5686e51025a5364180f14ed541ac7f
mathias.koehnke <mathias.koehnke@359e7cc3-0860-1fdf-adbe-24c20723d3c1>
2010-03-30 23:45:53 +0000

    initial JPA commit

--- core/src/main/resources/db_schema.sql
+++ core/src/main/resources/db_schema.sql
@@ -1,41 +1,35 @@
-CREATE TABLE t_audioitem ( 
-  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), 
-  uuid VARCHAR(255) );
+CREATE TABLE t_sequence ( 
+  seq_name VARCHAR(50), 
+  seq_count INT );
 
-CREATE TABLE t_locale ( 
-  id INT, 
-  language VARCHAR(255), 
-  country VARCHAR(100), 
-  description VARCHAR(2048) );
-
-CREATE TABLE t_manifest ( 
-  id INT );
+CREATE TABLE t_localized_string ( 
+  id INT PRIMARY KEY, 
+  string INT,
+  translation_string VARCHAR(2048), 
+  locale INT );
 
 CREATE TABLE t_audioitem_has_category ( 
   audioitem INT, 
-  category INT );
-
-CREATE TABLE t_category ( 
-  id INT, 
-  title VARCHAR(255), 
-  description VARCHAR(2048), 
-  parent INT );
+  category INT,
+  PRIMARY KEY(audioitem,category));
 
 CREATE TABLE t_referenced_file ( 
-  id INT, 
+  id INT PRIMARY KEY, 
   location VARCHAR(2048), 
   manifest INT );
 
-CREATE TABLE t_localized_audioitem ( 
-  id INT, 
+CREATE TABLE t_manifest ( 
+  id INT PRIMARY KEY);
+
+CREATE TABLE t_category ( 
+  id INT PRIMARY KEY, 
   uuid VARCHAR(255), 
-  language INT, 
-  location VARCHAR(2048), 
-  audioitem INT, 
-  manifest INT );
+  lang_title INT, 
+  lang_desc INT, 
+  parent INT );
 
 CREATE TABLE t_metadata ( 
-  id INT, 
+  id INT PRIMARY KEY, 
   dc_contributor VARCHAR(255), 
   dc_coverage VARCHAR(255), 
   dc_creator VARCHAR(100), 
@@ -57,3 +51,37 @@ CREATE TABLE t_metadata (
   lb_copy_count INT, 
   lb_play_count INT, 
   lb_rating SMALLINT );
+
+CREATE TABLE t_audioitem ( 
+  id INT PRIMARY KEY, 
+  uuid VARCHAR(255) );
+
+CREATE TABLE t_string ( 
+  id INT PRIMARY KEY, 
+  context VARCHAR(2048) );
+
+CREATE TABLE t_localized_audioitem ( 
+  id INT PRIMARY KEY, 
+  uuid VARCHAR(255), 
+  language INT, 
+  location VARCHAR(2048), 
+  audioitem INT, 
+  metadata INT, 
+  manifest INT );
+
+CREATE TABLE t_locale ( 
+  id INT PRIMARY KEY, 
+  language VARCHAR(255), 
+  country VARCHAR(100), 
+  description VARCHAR(2048) );
+  
+  
+INSERT INTO t_sequence(seq_name,seq_count) VALUES('gen_audioitem',0);
+INSERT INTO t_sequence(seq_name,seq_count) VALUES('gen_localized_audioitem',0);
+INSERT INTO t_sequence(seq_name,seq_count) VALUES('gen_category',0);
+INSERT INTO t_sequence(seq_name,seq_count) VALUES('gen_localized_string',0);
+INSERT INTO t_sequence(seq_name,seq_count) VALUES('gen_locale',0);
+INSERT INTO t_sequence(seq_name,seq_count) VALUES('gen_manifest',0);
+INSERT INTO t_sequence(seq_name,seq_count) VALUES('gen_metadata',0);
+INSERT INTO t_sequence(seq_name,seq_count) VALUES('gen_referenced_file',0);
+INSERT InTO t_sequence(seq_name,seq_count) VALUES('gen_string',0);
