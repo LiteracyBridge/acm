06a573c40c3163ff84007f62a49df5c99c44910f
michibusch <michibusch@359e7cc3-0860-1fdf-adbe-24c20723d3c1>
2010-04-23 19:18:33 +0000

    ResourceBundle for metafield labels and static class to access and iterate labels for different locals;  so far only one default bundle in English available.

---
+++ acm/trunk/gui/src/main/java/org/literacybridge/acm/resourcebundle/LabelProvider.java
@@ -0,0 +1,75 @@
+package org.literacybridge.acm.resourcebundle;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Locale;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+import org.literacybridge.acm.metadata.LBMetadataIDs;
+import org.literacybridge.acm.metadata.MetadataField;
+
+import com.google.common.collect.UnmodifiableIterator;
+
+public class LabelProvider {
+	public static final class KeyValuePair<K, V> {
+		private final K key;
+		private final V value;
+		
+		public KeyValuePair(K key, V value) {
+			this.key = key;
+			this.value = value;
+		}
+		
+		public K getKey() {return key;}
+		public V getValue() {return value;}
+	}
+	
+	private static final String BUNDLE_NAME = "labels";
+	private static final String METAFIELD_PROPERTY_PREFIX = "METAFIELD_";
+	
+	private final static Map<Locale, ResourceBundle> bundles = new HashMap<Locale, ResourceBundle>();
+	
+	private static final ResourceBundle getResourceBundle(Locale locale) {
+		ResourceBundle bundle = bundles.get(locale);
+		if (bundle == null) {
+			bundle = ResourceBundle.getBundle(BUNDLE_NAME, locale);
+			bundles.put(locale, bundle);
+		}
+		return bundle;
+	}
+	
+	public static String getLabel(MetadataField<?> field, Locale locale) {
+		int fieldID = LBMetadataIDs.FieldToIDMap.get(field);
+		String resourceName = METAFIELD_PROPERTY_PREFIX + fieldID;
+		
+		ResourceBundle bundle = getResourceBundle(locale);
+		return bundle.getString(resourceName);
+	}
+	
+	public static Iterator<KeyValuePair<MetadataField<?>, String>> getMetaFieldLabelsIterator(final Locale locale) {
+		final Iterator<MetadataField<?>> fieldIterator = LBMetadataIDs.FieldToIDMap.keySet().iterator();
+		return new UnmodifiableIterator<KeyValuePair<MetadataField<?>,String>>() {
+
+			@Override
+			public boolean hasNext() {
+				return fieldIterator.hasNext();
+			}
+
+			@Override
+			public KeyValuePair<MetadataField<?>, String> next() {
+				MetadataField<?> field = fieldIterator.next();
+				String value = getLabel(field, locale);
+				return new KeyValuePair<MetadataField<?>, String>(field, value);
+			}
+				
+		};
+	}
+	
+	public static void main(String[] args) {
+		Iterator<KeyValuePair<MetadataField<?>, String>> it = getMetaFieldLabelsIterator(Locale.ENGLISH);
+		while (it.hasNext()) {
+			System.out.println(it.next().getValue());
+		}
+	}
+}
