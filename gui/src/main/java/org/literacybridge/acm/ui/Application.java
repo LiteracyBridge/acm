9a2d89f1b9604e79b11fbf15b050bea93df0ba66
schade.sven <schade.sven@359e7cc3-0860-1fdf-adbe-24c20723d3c1>
2010-04-24 20:57:10 +0000

    AudioItem dialog added. Can be called via PopupMenu from audioItem table. Functionality missing as some model details must be discussed first...

--- gui/src/main/java/org/literacybridge/acm/ui/Application.java
+++ gui/src/main/java/org/literacybridge/acm/ui/Application.java
@@ -8,7 +8,7 @@ import org.literacybridge.acm.ui.ResourceView.ResourceView;
 import org.literacybridge.acm.ui.ResourceView.ToolbarView;
 import org.literacybridge.acm.util.SimpleMessageService;
 
-public class Application {
+public class Application extends JFrame {
 
 	// message pump
 	private static SimpleMessageService instance = new SimpleMessageService();
@@ -16,23 +16,29 @@ public class Application {
 	public static SimpleMessageService getMessageService() {
 		return instance;
 	};
+
+	public static Application application = new Application();
+	
+	private Application() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// toolbar view on top
+	    ToolbarView toolbarView = new ToolbarView();
+	    getContentPane().add(toolbarView, BorderLayout.PAGE_START);
+	    
+	    // Resource view
+	    ResourceView resourceView = new ResourceView();	    
+	    add(resourceView, BorderLayout.CENTER);
+	}
+	
+	public static Application getApplication() {
+		return application;
+	}
 	
 	
 	
 	public static void main(String[] args) {
-	    JFrame frame = new JFrame();
-	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-	    // toolbar view on top
-	    ToolbarView toolbarView = new ToolbarView();
-	    frame.getContentPane().add(toolbarView, BorderLayout.PAGE_START);
-	    
-	    // Resource view
-	    ResourceView resourceView = new ResourceView();	    
-	    frame.add(resourceView, BorderLayout.CENTER);
-	  
-	    frame.setSize(1000, 600);
-	    frame.setVisible(true);
-	}
-	
+		Application app = Application.getApplication();
+		app.setSize(1000, 600);
+		app.setVisible(true);
+	}	
 }
---
+++ gui/src/main/java/org/literacybridge/acm/ui/ResourceView/audioItems/AudioItemView.java
@@ -0,0 +1,111 @@
+package org.literacybridge.acm.ui.ResourceView.audioItems;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+import org.literacybridge.acm.api.IDataRequestResult;
+
+import org.literacybridge.acm.ui.Application;
+import org.literacybridge.acm.ui.dialogs.AudioItemPropertiesDialog;
+
+public class AudioItemView extends Container implements Observer {
+
+	private static final long serialVersionUID = -2886958461177831842L;
+
+	// model
+	private IDataRequestResult currResult = null;
+
+	// table
+	private JTable audioItemTable = null;
+	private JPopupMenu audioItemTablePopupMenu = null;
+
+	public AudioItemView() {
+		setLayout(new BorderLayout());
+		createTable();
+		createPopupMenu();
+		addHandlers();
+		Application.getMessageService().addObserver(this);
+	}
+
+	private void createTable() {
+		audioItemTable = new JTable();
+		updateTable(); // init empty
+
+		JScrollPane scrollPane = new JScrollPane(audioItemTable);
+		add(BorderLayout.CENTER, scrollPane);
+	}
+
+	private void updateTable() {
+		audioItemTable.setModel(new AudioItemTableModel(currResult));
+	}
+
+	private void addHandlers() {
+	    MouseListener popupListener = new PopupListener();
+	    audioItemTable.addMouseListener(popupListener);
+	    audioItemTable.getTableHeader().addMouseListener(popupListener);
+	}
+
+	private void createPopupMenu() {
+		audioItemTablePopupMenu = new JPopupMenu();
+		JMenuItem showPropertiesMenuItem = new JMenuItem("Properties...");
+		showPropertiesMenuItem
+				.addActionListener(new PopupMenuItemClicked(this));
+		audioItemTablePopupMenu.add(showPropertiesMenuItem);
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		if (arg instanceof IDataRequestResult) {
+			currResult = (IDataRequestResult) arg;
+			updateTable();
+		}
+	}
+
+	private class PopupListener extends MouseAdapter {
+		public void mousePressed(MouseEvent e) {
+			showPopup(e);
+		}
+
+		public void mouseReleased(MouseEvent e) {
+			showPopup(e);
+		}
+
+		private void showPopup(MouseEvent e) {
+			if (e.isPopupTrigger()) {
+				audioItemTablePopupMenu.show(e.getComponent(), e.getX(), e
+						.getY());
+			}
+		}
+	}
+
+	// Handler our popup action
+	private class PopupMenuItemClicked implements ActionListener {
+
+		private AudioItemView adaptee = null;
+
+		public PopupMenuItemClicked(AudioItemView adaptee) {
+			this.adaptee = adaptee;
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			int index = adaptee.audioItemTable.getSelectedRow();
+			AudioItemPropertiesDialog dlg = new AudioItemPropertiesDialog(
+					Application.getApplication(), currResult.getAudioItems(),
+					index);
+			dlg.setVisible(true);
+		}
+	}
+}
