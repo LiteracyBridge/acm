3bc886c2134d5181adb24555d8bb7e8ab8f1d3aa
michibusch <michibusch@359e7cc3-0860-1fdf-adbe-24c20723d3c1>
2010-04-26 16:43:41 +0000

    Add name variable to MetadataField to make Metadata.toString() implementation more convenient.

--- core/src/main/java/org/literacybridge/acm/metadata/MetadataSpecification.java
+++ core/src/main/java/org/literacybridge/acm/metadata/MetadataSpecification.java
@@ -4,12 +4,14 @@ import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 import java.util.Collection;
+import java.util.Map;
 
 import org.literacybridge.acm.metadata.types.MetadataIntegerField;
 import org.literacybridge.acm.metadata.types.MetadataStringField;
 
 import com.google.common.collect.Collections2;
 import com.google.common.collect.ImmutableCollection;
+import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 
 public interface MetadataSpecification {
@@ -21,7 +23,7 @@ public interface MetadataSpecification {
 		Content: The title of the DTB, including any subtitles.
 		Occurrence: Required
 	*/
-	public final static MetadataField<String> DC_TITLE = new MetadataStringField();
+	public final static MetadataField<String> DC_TITLE = new MetadataStringField("DC_TITLE");
 	
 	/** dc:Creator 
 		Content: Names of primary author or creator of the intellectual content of the publication.
@@ -32,27 +34,27 @@ public interface MetadataSpecification {
 	 */
 	public final static Attribute<String> DC_CREATOR_ROLE = new Attribute<String>();
 	public final static Attribute<String> DC_CREATOR_FILE_AS = new Attribute<String>();
-	public final static MetadataField<String> DC_CREATOR = new MetadataStringField(DC_CREATOR_ROLE, DC_CREATOR_FILE_AS);
+	public final static MetadataField<String> DC_CREATOR = new MetadataStringField("DC_CREATOR", DC_CREATOR_ROLE, DC_CREATOR_FILE_AS);
 
 
 	/** dc:Subject 
 		Content: The topic of the content of the publication.
 		Occurrence: Optional - recommended.
 	 */ 
-	public final static MetadataField<String> DC_SUBJECT = new MetadataStringField();
+	public final static MetadataField<String> DC_SUBJECT = new MetadataStringField("DC_SUBJECT");
 	
 
 	/** dc:Description 
 		Content: Plain text describing the publication's content.
 		Occurrence: Optional
 	 */
-	public final static MetadataField<String> DC_DESCRIPTION = new MetadataStringField();
+	public final static MetadataField<String> DC_DESCRIPTION = new MetadataStringField("DC_DESCRIPTION");
 
 	/** dc:Publisher 
 		Content: The agency responsible for making the DTB available. (Compare dtb:sourcePublisher and dtb:producer.)
 		Occurrence: Required
 	 */
-	public final static MetadataField<String> DC_PUBLISHER = new MetadataStringField();
+	public final static MetadataField<String> DC_PUBLISHER = new MetadataStringField("DC_PUBLISHER");
 
 	/** dc:Contributor 
 		Content: A party whose contribution to the publication is secondary to those named in dc:Creator.
@@ -63,7 +65,7 @@ public interface MetadataSpecification {
 	*/
 	public final static Attribute<String> DC_CONTRIBUTOR_ROLE = new Attribute<String>();
 	public final static Attribute<String> DC_CONTRIBUTOR_FILE_AS  = new Attribute<String>();
-	public final static MetadataField<String> DC_CONTRIBUTOR = new MetadataStringField(DC_CONTRIBUTOR_ROLE, DC_CONTRIBUTOR_FILE_AS);
+	public final static MetadataField<String> DC_CONTRIBUTOR = new MetadataStringField("DC_CONTRIBUTOR", DC_CONTRIBUTOR_ROLE, DC_CONTRIBUTOR_FILE_AS);
 
 
 	/** dc:Date 
@@ -75,7 +77,7 @@ public interface MetadataSpecification {
 			   		   Best practice is to use dtb:revision and dtb:revisionDate instead. 
 	*/
 	public final static Attribute<String> DC_DATE_EVENT = new Attribute<String>();
-	public final static MetadataField<ISO8601Date> DC_DATE = new MetadataField<ISO8601Date>(DC_DATE_EVENT) {
+	public final static MetadataField<ISO8601Date> DC_DATE = new MetadataField<ISO8601Date>("DC_DATE", DC_DATE_EVENT) {
 		@Override
 	    void validateValue(ISO8601Date value) throws InvalidMetadataException {
 			// TODO: implement
@@ -98,13 +100,13 @@ public interface MetadataSpecification {
 		Content: The nature of the content of the DTB (i.e., sound, text, image). Best practice is to draw from the Dublin Core's enumerated list [DC-Type].
 		Occurrence: Optional
 	 */ 
-	public final static MetadataField<String> DC_TYPE = new MetadataStringField();
+	public final static MetadataField<String> DC_TYPE = new MetadataStringField("DC_TYPE");
 
 	/** dc:Format 
 		Content: The standard or specification to which the DTB was produced. Values of dc:Format in a DTB conforming to this standard are valid only if they read "ANSI/NISO Z39.86-2005".
 		Occurrence: Required
 	 */
-	public final static MetadataField<String> DC_FORMAT = new MetadataStringField();
+	public final static MetadataField<String> DC_FORMAT = new MetadataStringField("DC_FORMAT");
 
 	/** dc:Identifier 
 		Content: A string or number identifying the DTB. One instance of this element, that which is referenced from the packageunique-identifier attribute, must include an id.
@@ -113,19 +115,19 @@ public interface MetadataSpecification {
 	 		* scheme -- (optional) The name of the system or authority that generated or assigned the identifier. For example, "DOI", "ISBN", or "DTB".
 	 */  
 	public final static Attribute<String> DC_IDENTIFIER_SCHEME = new Attribute<String>();
-	public final static MetadataField<String> DC_IDENTIFIER = new MetadataStringField(DC_IDENTIFIER_SCHEME);
+	public final static MetadataField<String> DC_IDENTIFIER = new MetadataStringField("DC_IDENTIFIER", DC_IDENTIFIER_SCHEME);
 
 	/** dc:Source 
 		Content: A reference to a resource (e.g., a print original, ebook, etc.) from which the DTB is derived. Best practice is to use the ISBN when available.
 		Occurrence: Optional - recommended.
 	 */ 
-	public final static MetadataField<String> DC_SOURCE = new MetadataStringField();
+	public final static MetadataField<String> DC_SOURCE = new MetadataStringField("DC_SOURCE");
 
 	/** dc:Language 
 		Content: Language of the content of the publication. An [RFC 3066] language code. For Sweden: "sv" or "sv-SE"; for UK: "en" or "en-GB"; for US: "en" or "en-US"; etc.
 		Occurrence: Required
 	 */ 
-	public final static MetadataField<RFC3066LanguageCode> DC_LANGUAGE = new MetadataField<RFC3066LanguageCode>() {
+	public final static MetadataField<RFC3066LanguageCode> DC_LANGUAGE = new MetadataField<RFC3066LanguageCode>("DC_LANGUAGE") {
 		@Override
 		void validateValue(RFC3066LanguageCode value) throws InvalidMetadataException {
 			RFC3066LanguageCode.validate(value); 
@@ -148,19 +150,19 @@ public interface MetadataSpecification {
 		Content: A reference to a related resource.
 		Occurrence: Optional
 	 */ 
-	public final static MetadataField<String> DC_RELATION = new MetadataStringField();
+	public final static MetadataField<String> DC_RELATION = new MetadataStringField("DC_RELATION");
 
 	/** dc:Coverage 
 		Content: The extent or scope of the content of the resource. Not expected to be used for DTBs.
 		Occurrence: Optional
 	 */ 
-	public final static MetadataField<String> DC_COVERAGE = new MetadataStringField();
+	public final static MetadataField<String> DC_COVERAGE = new MetadataStringField("DC_COVERAGE");
 
 	/** dc:Rights 
 		Content: Information about rights held in and over the DTB. (Compare dtb:sourceRights.)
 		Occurrence: Optional
 	 */ 
-	public final static MetadataField<String> DC_RIGHTS  = new MetadataStringField();
+	public final static MetadataField<String> DC_RIGHTS  = new MetadataStringField("DC_RIGHTS");
 
 	//============================================================================================================
 	// Additional Daisy dtb metadata fields
@@ -170,30 +172,30 @@ public interface MetadataSpecification {
     	Content: Non-negative integer value of the specific version of the DTB. Incremented each time the DTB is revised.
     	Occurrence: Optional. Not repeatable.
      */
-	public final static MetadataField<String> DTB_REVISION = new MetadataStringField();
+	public final static MetadataField<String> DTB_REVISION = new MetadataStringField("DTB_REVISION");
 	
     /** dtb:revisionDate 
     	Content: Date associated with the specific dtb:revision. In format from [ISO8601]; the syntax is YYYY[-MM[-DD]] with a mandatory 4-digit year, an optional 2-digit month, and, if the month is present, an optional 2-digit day of month.
     	Occurrence: Optional. Not repeatable.
 	 */
-	public final static MetadataField<String> DTB_REVISION_DATE = new MetadataStringField();
+	public final static MetadataField<String> DTB_REVISION_DATE = new MetadataStringField("DTB_REVISION_DATE");
 	
     /** dtb:revisionDescription 
     	Content: A string describing the changes introduced in a specific dtb:revision.
     	Occurrence: Optional. Not repeatable.
 	 */ 
-	public final static MetadataField<String> DTB_REVISION_DESCRIPTION = new MetadataStringField();
+	public final static MetadataField<String> DTB_REVISION_DESCRIPTION = new MetadataStringField("DTB_REVISION_DESCRIPTION");
 	
 
 	//============================================================================================================
 	// Additional Literacy Bridge fields
 	//============================================================================================================
 	
-	public final static MetadataField<Integer> LB_COPY_COUNT = new MetadataIntegerField();
+	public final static MetadataField<Integer> LB_COPY_COUNT = new MetadataIntegerField("LB_COPY_COUNT");
 	
-	public final static MetadataField<Integer> LB_PLAY_COUNT = new MetadataIntegerField();
+	public final static MetadataField<Integer> LB_PLAY_COUNT = new MetadataIntegerField("LB_PLAY_COUNT");
 	
-	public final static MetadataField<Integer> LB_RATING = new MetadataIntegerField();
+	public final static MetadataField<Integer> LB_RATING = new MetadataIntegerField("LB_RATING");
 
 
 	//============================================================================================================
@@ -224,3 +226,4 @@ public interface MetadataSpecification {
 																		.add(LB_RATING)
 																		.build();
 }
+	
