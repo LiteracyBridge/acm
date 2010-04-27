3b37e17436c332ac932c5f6832c8823b364afe7a
schade.sven <schade.sven@359e7cc3-0860-1fdf-adbe-24c20723d3c1>
2010-04-27 21:03:21 +0000

    Tree shows now a new section for device. Device mounted will be shown as tree node and remove after unmount.

--- acm/trunk/gui/src/main/java/org/literacybridge/acm/ui/Application.java
+++ acm/trunk/gui/src/main/java/org/literacybridge/acm/ui/Application.java
@@ -4,20 +4,39 @@ import java.awt.BorderLayout;
 
 import javax.swing.JFrame;
 
+import org.literacybridge.acm.device.FileSystemMonitor;
+import org.literacybridge.acm.device.LiteracyBridgeTalkingBookRecognizer;
 import org.literacybridge.acm.ui.ResourceView.ResourceView;
 import org.literacybridge.acm.ui.ResourceView.ToolbarView;
 import org.literacybridge.acm.util.SimpleMessageService;
 
 public class Application extends JFrame {
 
+
+	
+	
 	// message pump
-	private static SimpleMessageService instance = new SimpleMessageService();
+	private static SimpleMessageService simpleMessageService = new SimpleMessageService();
 	
 	public static SimpleMessageService getMessageService() {
-		return instance;
+		return simpleMessageService;
 	};
 
+
+	// file system monitor for the audio devices
+	private static FileSystemMonitor fileSystemMonitor = new FileSystemMonitor();
+	
+	public static FileSystemMonitor getFileSystemMonitor() {
+		return fileSystemMonitor;
+	}
+	
+	
+	// application instance
 	public static Application application = new Application();
+	
+	public static Application getApplication() {
+		return application;
+	}
 	
 	private Application() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -28,12 +47,11 @@ public class Application extends JFrame {
 	    // Resource view
 	    ResourceView resourceView = new ResourceView();	    
 	    add(resourceView, BorderLayout.CENTER);
+	    
+	    // starts file system monitor after UI has been initialized
+		fileSystemMonitor.addDeviceRecognizer(new LiteracyBridgeTalkingBookRecognizer());	
+		fileSystemMonitor.start();
 	}
-	
-	public static Application getApplication() {
-		return application;
-	}
-	
 	
 	
 	public static void main(String[] args) {
--- acm/trunk/gui/src/main/java/org/literacybridge/acm/ui/ResourceView/CategoryView.java
+++ acm/trunk/gui/src/main/java/org/literacybridge/acm/ui/ResourceView/CategoryView.java
@@ -6,14 +6,21 @@ import it.cnr.imaa.essi.lablib.gui.checkboxtree.TreeCheckingListener;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.swing.JScrollPane;
+import javax.swing.SwingUtilities;
 import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.TreePath;
 
 import org.literacybridge.acm.api.IDataRequestResult;
 import org.literacybridge.acm.categories.Taxonomy.Category;
-
+import org.literacybridge.acm.core.MessageBus;
+import org.literacybridge.acm.core.MessageBus.Message;
+import org.literacybridge.acm.device.DeviceConnectEvent;
+import org.literacybridge.acm.device.DeviceInfo;
 import org.literacybridge.acm.resourcebundle.LabelProvider;
 import org.literacybridge.acm.ui.Application;
 import org.literacybridge.acm.util.LanguageUtil;
@@ -26,7 +33,15 @@ public class CategoryView extends Container {
 	private IDataRequestResult result = null;
 	// tree
 	private CheckboxTree categoryTree = null;
-
+	// root nodes
+	private DefaultMutableTreeNode rootNode = null;
+	private DefaultMutableTreeNode categoryRootNode = null;
+	private DefaultMutableTreeNode deviceRootNode = null;
+	
+	// list of available devices
+	private Map<String, DefaultMutableTreeNode> deviceUidtoTreeNodeMap = new HashMap<String, DefaultMutableTreeNode>();
+	
+	
 	public CategoryView(IDataRequestResult result) {
 		this.result = result;
 		createControls();
@@ -41,27 +56,35 @@ public class CategoryView extends Container {
 	}
 
 	private void createTree() {
-		DefaultMutableTreeNode root = new DefaultMutableTreeNode(
+		rootNode = new DefaultMutableTreeNode("Root node");
+		categoryRootNode = new DefaultMutableTreeNode(
 				LabelProvider.getLabel(LabelProvider.CATEGORY_ROOT_LABEL, LanguageUtil.getUserChoosenLanguage()));
+		rootNode.add(categoryRootNode);
 		
+		// add all categories
 		Category rootCategory = result.getRootCategory();
 		if (rootCategory.hasChildren()) {
 			for (Category c : rootCategory.getChildren()) {
-				addChildNodes(root, c);
+				addChildNodes(categoryRootNode, c);
 			}
 		}
-		categoryTree = new CheckboxTree(root);
 		
-		addListeners();
+		deviceRootNode = new DefaultMutableTreeNode("Devices");
+		rootNode.add(deviceRootNode);
+		
+		categoryTree = new CheckboxTree(rootNode);
+		categoryTree.setRootVisible(false);
+		
+		addListeners(); // at last
 	}
 
 	private void addListeners() {
 		categoryTree.addTreeCheckingListener(new TreeCheckingListener() {
 			public void valueChanged(TreeCheckingEvent e) {
 				TreePath[] tp = categoryTree.getCheckingPaths();
-				for (int i = 0; i < tp.length; i++) {
-					System.out.println("Checked paths changed: user clicked on " + tp[i]);					
-				}
+//				for (int i = 0; i < tp.length; i++) {
+//					System.out.println("Checked paths changed: user clicked on " + tp[i]);					
+//				}
 
 				/*
 				 * ADD CODE THAT GETS AUDIO ITEMS FROM BACKEND FOR SELECTED CATEGORIES ....
@@ -72,6 +95,8 @@ public class CategoryView extends Container {
 				Application.getMessageService().pumpMessage(result);	
 			}
 		});
+		
+		addAudioDeviceListener();
 	}
 
 	private void addChildNodes(DefaultMutableTreeNode parent, Category category) {
@@ -83,5 +108,46 @@ public class CategoryView extends Container {
 				addChildNodes(child, c);
 			}
 		}
+	}
+	
+	private void addDeviceNode(DefaultMutableTreeNode parent, final DeviceInfo deviceInfo) {
+		if (parent != null) {
+			String deviceID = deviceInfo.getDeviceUID();
+			if (deviceUidtoTreeNodeMap.containsKey(deviceID)) {
+				// already existing, assume device was unplugged
+				DefaultTreeModel model = (DefaultTreeModel) categoryTree.getModel();
+				DefaultMutableTreeNode node = deviceUidtoTreeNodeMap.get(deviceID);
+				model.removeNodeFromParent(node);
+				deviceUidtoTreeNodeMap.remove(deviceID);
+			} else {
+				// new device found
+				DefaultMutableTreeNode child = new DefaultMutableTreeNode(deviceInfo.getPathToDevice());
+				parent.add(child);
+				categoryTree.setSelectionPath(new TreePath(child.getPath()));	
+				deviceUidtoTreeNodeMap.put(deviceID, child);
+			}
+		}
+	}
+	
+	private void addAudioDeviceListener() {
+		MessageBus bus = MessageBus.getInstance();
+		bus.addListener(DeviceConnectEvent.class, new MessageBus.MessageListener() {
+			
+			@Override
+			public void receiveMessage(final Message message) {
+				
+				if (message instanceof DeviceConnectEvent) {
+					final DeviceConnectEvent dce = (DeviceConnectEvent) message;
+					Runnable addDeviceToList = new Runnable() {					
+						@Override
+						public void run() {
+							addDeviceNode(deviceRootNode, dce.getDeviceInfo());						
+						}
+					};
+					
+					SwingUtilities.invokeLater(addDeviceToList);	
+				}
+			}
+		});
 	}
 }
