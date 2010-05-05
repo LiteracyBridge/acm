41a49ccea44ec76b82af802e7c02e80dc5fa81bf
michibusch <michibusch@359e7cc3-0860-1fdf-adbe-24c20723d3c1>
2010-05-05 09:11:29 +0000

    Serialization of categories; a18 import

--- core/src/test/java/org/literacybridge/acm/metadata/TestMetadataSerialization.java
+++ core/src/test/java/org/literacybridge/acm/metadata/TestMetadataSerialization.java
@@ -4,34 +4,54 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 import junit.framework.TestCase;
 
+import org.literacybridge.acm.categories.Taxonomy;
+import org.literacybridge.acm.categories.Taxonomy.Category;
+import org.literacybridge.acm.db.Persistence;
+import org.literacybridge.acm.db.PersistentCategory;
+
 public class TestMetadataSerialization extends TestCase {
+	@Override
+	public void setUp() throws Exception {
+		Persistence.initialize();
+	}
+	
 	public void testSerialization() throws Exception {
 		Metadata metadata = new Metadata();
+		Set<Category> categories = new HashSet<Category>();
+		
+		Taxonomy.getTaxonomy();
+		categories.add(new Category(PersistentCategory.getFromDatabase("1")));
 		
 		// add some metadata
 		metadata.addMetadataField(MetadataSpecification.DC_TITLE, new MetadataValue<String>("test"));
+		metadata.addMetadataField(MetadataSpecification.DC_TITLE, new MetadataValue<String>("test1"));
+		
+		metadata.addMetadataField(MetadataSpecification.DC_CREATOR, new MetadataValue<String>("test2"));
 		
 		
 		// validate
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		DataOutputStream out = new DataOutputStream(baos);
 		LBMetadataSerializer serializer = new LBMetadataSerializer();
-		serializer.serialize(metadata, out);
+		serializer.serialize(categories, metadata, out);
 		out.close();
 		
+		Set<Category> readCategories = new HashSet<Category>();
 		byte[] buffer = baos.toByteArray();
 		DataInputStream in = new DataInputStream(new ByteArrayInputStream(buffer));
-		Metadata deserialized = serializer.deserialize(in);
+		Metadata deserialized = serializer.deserialize(readCategories, in);
 		
 		assertEquals("Mismatch in deserialized metadata.", metadata, deserialized);
 	}
 	
-	public static void assertEquals(String message, Metadata expected, Metadata actual) {
+	public static void assertEquals(String message, Metadata expected, Metadata actual) throws Exception {
 		Iterator<MetadataField<?>> it = expected.getFieldsIterator();
 		while (it.hasNext()) {
 			MetadataField<?> field = it.next();
@@ -39,7 +59,8 @@ public class TestMetadataSerialization extends TestCase {
 		}
 	}
 
-	public static <T> void assertEquals(String message, MetadataField<T> field, Metadata expected, Metadata actual) {
+	public static <T> void assertEquals(String message, MetadataField<T> field, Metadata expected, Metadata actual) 
+			throws Exception {
 		List<MetadataValue<T>> valuesExpected = expected.getMetadataValues(field);
 		List<MetadataValue<T>> valuesActual   = actual.getMetadataValues(field);
 		
