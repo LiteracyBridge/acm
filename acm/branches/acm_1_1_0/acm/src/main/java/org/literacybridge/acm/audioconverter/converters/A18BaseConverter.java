63ee49b462286e4806ebb7b12ea25024fe8240cb
michibusch@gmail.com <michibusch@gmail.com@359e7cc3-0860-1fdf-adbe-24c20723d3c1>
2013-07-15 23:25:23 +0000

    Change a18 converter to copy source file into tmp directory prior to conversion.

--- acm/branches/acm_1_1_0/acm/src/main/java/org/literacybridge/acm/audioconverter/converters/A18BaseConverter.java
+++ acm/branches/acm_1_1_0/acm/src/main/java/org/literacybridge/acm/audioconverter/converters/A18BaseConverter.java
@@ -1,11 +1,15 @@
 package org.literacybridge.acm.audioconverter.converters;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.Map;
+
+import org.literacybridge.acm.gui.util.UIUtils;
+import org.literacybridge.acm.utils.IOUtils;
 
 public abstract class A18BaseConverter extends BaseAudioConverter {
 
@@ -32,21 +36,40 @@ public abstract class A18BaseConverter extends BaseAudioConverter {
 	
 	@Override
 	public ConversionResult doConvertFile(File audioFile, File targetDir, File targetFile, File tmpDir, Map<String, String> parameters) throws ConversionException {
-	
-		String cmd = getCommand(audioFile, targetDir, parameters);
-		ConversionResult result = new ConversionResult();
-		result.outputFile = new File(targetDir, audioFile.getName() + targetFormatExtension);
-		result.response = BaseAudioConverter.executeConversionCommand(cmd,
-														  false,  // important! a18 converter prints to stdout, not stderr
-														  audioFile.getName());
-	
-		Matcher matcher = pattern.matcher(result.response);
-		if (matcher.matches()) {
-			if (!result.response.endsWith("Succeeded")) {
-				throw new ConversionException("Converter: Internal error while converting file: '" + audioFile.getName() + "'");
+		File tmpSourceFile = null;
+		
+		try {
+			if (tmpDir != null && !tmpDir.equals(audioFile.getParentFile())) {
+				tmpSourceFile = new File(tmpDir, audioFile.getName());
+				try {
+					IOUtils.copy(audioFile, tmpSourceFile);
+					// the a18 converter stores intermediate files in the source directory, which is often undesired (e.g. when using dropbox)
+					// copy the source file to a temp directory first
+					audioFile = tmpSourceFile;
+				} catch (IOException e) {
+					// ignore and use original source file
+				}
+			}
+			
+			String cmd = getCommand(audioFile, targetDir, parameters);
+			ConversionResult result = new ConversionResult();
+			result.outputFile = new File(targetDir, audioFile.getName() + targetFormatExtension);
+			result.response = BaseAudioConverter.executeConversionCommand(cmd,
+															  false,  // important! a18 converter prints to stdout, not stderr
+															  audioFile.getName());
+		
+			Matcher matcher = pattern.matcher(result.response);
+			if (matcher.matches()) {
+				if (!result.response.endsWith("Succeeded")) {
+					throw new ConversionException("Converter: Internal error while converting file: '" + audioFile.getName() + "'");
+				}
+			}
+			return result;
+		} finally {
+			if (tmpSourceFile != null && tmpSourceFile.exists()) {
+				tmpSourceFile.delete();
 			}
 		}
-		return result;
 	}
 
 	@Override
