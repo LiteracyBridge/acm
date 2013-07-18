6be8c46581207c601cd130af467b44fa271e4d34
michibusch@gmail.com <michibusch@gmail.com@359e7cc3-0860-1fdf-adbe-24c20723d3c1>
2013-07-18 08:17:26 +0000

    Fix NPE in A18DurationUtil

--- acm/branches/acm_1_1_0/acm/src/main/java/org/literacybridge/acm/repository/A18DurationUtil.java
+++ acm/branches/acm_1_1_0/acm/src/main/java/org/literacybridge/acm/repository/A18DurationUtil.java
@@ -16,23 +16,25 @@ import org.literacybridge.acm.utils.IOUtils;
 public class A18DurationUtil {
 	public static void updateDuration(AudioItem audioItem) throws IOException {		
     	File f = Configuration.getRepository().getAudioFile(audioItem, AudioFormat.A18);
-    	DataInputStream in = new DataInputStream(new BufferedInputStream(new FileInputStream(f)));
-    	in.skipBytes(4);
-    	int bps = IOUtils.readLittleEndian16(in);
-    	in.close();
-    	long sec = (f.length() * 8 + bps/2) / bps;
-    	int min = (int)(sec / 60L);
-    	//String test = String.valueOf(sec) + "/" + String.valueOf(bps) + "/" + String.valueOf(f.length());
-    	sec -= min * 60;
-    	String sMin = String.valueOf(min);
-    	String sSec = String.valueOf(sec);
-    	if (sMin.length()==1)
-    		sMin = "0" + sMin;
-    	if (sSec.length()==1)
-    		sSec = "0" + sSec;
-    	String duration = sMin + ":" + sSec + ((bps==16000)?"  l":"  h");
-    	//duration += test;
-    	audioItem.getLocalizedAudioItem(null).getMetadata().setMetadataField(MetadataSpecification.LB_DURATION, new MetadataValue<String>(duration));
-    	audioItem.commit();
+    	if (f != null) {
+	    	DataInputStream in = new DataInputStream(new BufferedInputStream(new FileInputStream(f)));
+	    	in.skipBytes(4);
+	    	int bps = IOUtils.readLittleEndian16(in);
+	    	in.close();
+	    	long sec = (f.length() * 8 + bps/2) / bps;
+	    	int min = (int)(sec / 60L);
+	    	//String test = String.valueOf(sec) + "/" + String.valueOf(bps) + "/" + String.valueOf(f.length());
+	    	sec -= min * 60;
+	    	String sMin = String.valueOf(min);
+	    	String sSec = String.valueOf(sec);
+	    	if (sMin.length()==1)
+	    		sMin = "0" + sMin;
+	    	if (sSec.length()==1)
+	    		sSec = "0" + sSec;
+	    	String duration = sMin + ":" + sSec + ((bps==16000)?"  l":"  h");
+	    	//duration += test;
+	    	audioItem.getLocalizedAudioItem(null).getMetadata().setMetadataField(MetadataSpecification.LB_DURATION, new MetadataValue<String>(duration));
+	    	audioItem.commit();
+    	}
 	}
 }
