bae4ecbca36c558990c925d2e6b11cb9168db805
cliffschmidt@gmail.com <cliffschmidt@gmail.com@359e7cc3-0860-1fdf-adbe-24c20723d3c1>
2013-08-12 21:58:29 +0000

    just updating ACM_VERSION constant to match svn revision after committing release branch

--- acm/branches/acm_1_1_0/acm/src/main/java/org/literacybridge/acm/Constants.java
+++ acm/branches/acm_1_1_0/acm/src/main/java/org/literacybridge/acm/Constants.java
@@ -3,7 +3,7 @@ package org.literacybridge.acm;
 import java.io.File;
 
 public class Constants {
-	public final static String ACM_VERSION					= "v1.0r1097";
+	public final static String ACM_VERSION					= "v1.0r1104";
 	public final static String LiteracybridgeHomeDirName	= "LiteracyBridge";
 	public final static String ACM_DIR_NAME			    	= "ACM";
 	public final static String CACHE_DIR_NAME			    = "cache";
