9b5689c7f4175810715eddde3f8cda5eb0a0b0f8
michibusch <michibusch@359e7cc3-0860-1fdf-adbe-24c20723d3c1>
2013-08-14 18:12:01 +0000

    Removing obsolete acm_1_1_0 branch (merged to trunk yesterday)

--- acm/branches/acm_1_1_0/acm/src/main/java/org/literacybridge/acm/device/DeviceRecognizer.java
+++
@@ -1,7 +0,0 @@
-package org.literacybridge.acm.device;
-
-import java.io.File;
-
-public abstract class DeviceRecognizer {
-	public abstract DeviceInfo identifyDevice(File pathToDevice);
-}
--- acm/branches/acm_1_1_0/acm/src/main/java/org/literacybridge/acm/gui/messages/RequestAudioItemToPlayMessage.java
+++
@@ -1,9 +0,0 @@
-package org.literacybridge.acm.gui.messages;
-
-public class RequestAudioItemToPlayMessage extends RequestAudioItemMessage {
-
-	public RequestAudioItemToPlayMessage(RequestType type) {
-		super(type);
-	}
-
-}
