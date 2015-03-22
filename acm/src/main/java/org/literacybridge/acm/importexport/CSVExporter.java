d506d77356bb2ff9f77f68d413655e847ba14428
michael <michael@literacybridge.org>
2015-03-21 18:28:57 -0700

    add CSVDatabaseExporter

--- acm/src/main/java/org/literacybridge/acm/importexport/CSVExporter.java
+++ acm/src/main/java/org/literacybridge/acm/importexport/CSVExporter.java
@@ -12,6 +12,7 @@ import org.literacybridge.acm.gui.util.UIUtils;
 import org.literacybridge.acm.metadata.LBMetadataIDs;
 import org.literacybridge.acm.metadata.Metadata;
 import org.literacybridge.acm.metadata.MetadataField;
+import org.literacybridge.acm.metadata.MetadataSpecification;
 import org.literacybridge.acm.metadata.MetadataValue;
 
 import au.com.bytecode.opencsv.CSVWriter;
@@ -20,11 +21,15 @@ import com.google.common.collect.Lists;
 
 public class CSVExporter {
 	private final static String CATEGORY_COLUMN_NAME = "CATEGORIES";
-	private final static String PLAYLIST_COLUMN_NAME = "PLAYLISTS";
-	
+	private final static String QUALITY_COLUMN_NAME = "QUALITY";
+
 	public static void export(LocalizedAudioItem[] audioItems, File targetFile) throws IOException {
+		export(Lists.newArrayList(audioItems), targetFile);
+	}
+
+	public static void export(Iterable<LocalizedAudioItem> audioItems, File targetFile) throws IOException {
 		CSVWriter writer = new CSVWriter(new FileWriter(targetFile), ',');
-		
+
 		List<MetadataField<?>> columns = Lists.newArrayList(LBMetadataIDs.FieldToIDMap.keySet());
 		// create a stable sort order based on the metadata field ids
 		Collections.sort(columns, new Comparator<MetadataField<?>>() {
@@ -32,41 +37,51 @@ public class CSVExporter {
 				return LBMetadataIDs.FieldToIDMap.get(o1).compareTo(LBMetadataIDs.FieldToIDMap.get(o2));
 			}
 		});
-		
+
 		// +2 for categories and playlists
 		final int numColumns = columns.size() + 2;
 		String[] values = new String[numColumns];
-		
+
 		// first write header
 		for (int i = 0; i < numColumns - 2; i++) {
 			values[i] = columns.get(i).getName();
 		}
-		
+
 		values[numColumns - 2] = CATEGORY_COLUMN_NAME;
-		values[numColumns - 1] = PLAYLIST_COLUMN_NAME;
-		
+		values[numColumns - 1] = QUALITY_COLUMN_NAME;
+
 		writer.writeNext(values);
-		
+
 		for (LocalizedAudioItem audioItem : audioItems) {
 			Metadata metadata = audioItem.getMetadata();
+			String quality = "l";
 			for (int i = 0; i < numColumns - 2; i++) {
 				MetadataField<?> field = columns.get(i);
-				values[i] = getStringValue(metadata, field);
+				String value = getStringValue(metadata, field);
+				if (field == MetadataSpecification.LB_DURATION) {
+					// last character is the quality
+					quality = value.substring(value.length() - 1);
+					int durationInSeconds =
+							Integer.parseInt(value.substring(0, 2)) * 60
+							+ Integer.parseInt(value.substring(3, 5));
+					value = Integer.toString(durationInSeconds);
+				}
+				values[i] = value;
 			}
 			values[numColumns - 2] = UIUtils.getCategoryListAsString(audioItem.getParentAudioItem());
-			values[numColumns - 1] = UIUtils.getPlaylistAsString(audioItem.getParentAudioItem());
+			values[numColumns - 1] = quality;
 			writer.writeNext(values);
 		}
-		
+
 		writer.close();
 	}
-	
+
 	private static <T> String getStringValue(Metadata metadata, MetadataField<T> field) {
 		List<MetadataValue<T>> values = metadata.getMetadataValues(field);
 		if (values == null || values.isEmpty()) {
 			return "";
 		}
-		
+
 		StringBuilder builder = new StringBuilder();
 		for (int i = 0; i < values.size(); i++) {
 			builder.append(values.get(i).getValue());
@@ -74,7 +89,7 @@ public class CSVExporter {
 				builder.append(",");
 			}
 		}
-		
+
 		return builder.toString();
 	}
 }
