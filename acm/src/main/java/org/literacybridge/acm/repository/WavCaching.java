d665847353552a50f4f9e989423e1bbfe7c8431c
michael <michael@literacybridge.org>
2015-04-26 19:17:58 -0700

    fixes

--- acm/src/main/java/org/literacybridge/acm/repository/WavCaching.java
+++ acm/src/main/java/org/literacybridge/acm/repository/WavCaching.java
@@ -13,64 +13,64 @@ import org.literacybridge.acm.repository.AudioItemRepository.AudioFormat;
 
 public class WavCaching {
 
-	private Set<AudioItem> uncachedAudioItems = new HashSet<AudioItem>();
+    private Set<AudioItem> uncachedAudioItems = new HashSet<AudioItem>();
 
-	public void cacheNewA18Files() {
-		findUncachedWaveFiles();
-		Application.getApplication().getTaskManager().execute(new Task());
-	}
+    public void cacheNewA18Files() {
+        findUncachedWaveFiles();
+        Application.getApplication().getTaskManager().execute(new Task());
+    }
 
-	private void findUncachedWaveFiles() {
-		System.out.print("Finding uncached wave files...");
-		AudioItemRepository repository = ACMConfiguration.getCurrentDB()
-				.getRepository();
+    private void findUncachedWaveFiles() {
+        System.out.print("Finding uncached wave files...");
+        AudioItemRepository repository = ACMConfiguration.getCurrentDB()
+                .getRepository();
 
-		for (AudioItem audioItem : AudioItem.getFromDatabase()) {
-			if (!repository.hasAudioItemFormat(audioItem, AudioFormat.WAV)) {
-				uncachedAudioItems.add(audioItem);
-			}
-		}
-		System.out.println("done");
-	}
+        for (AudioItem audioItem : AudioItem.getFromDatabase()) {
+            if (!repository.hasAudioItemFormat(audioItem, AudioFormat.WAV)) {
+                uncachedAudioItems.add(audioItem);
+            }
+        }
+        System.out.println("done");
+    }
 
-	private class Task extends ExtendedSwingWorker<Void, Void> {
-		String itemBeingProcessed;
+    private class Task extends ExtendedSwingWorker<Void, Void> {
+        String itemBeingProcessed;
 
-		/*
-		 * Main task. Executed in background thread.
-		 */
-		@Override
-		public Void doInBackground() {
-			int progress = 0;
-			// Initialize progress property.
-			setProgress(0);
-			Iterator<AudioItem> it = uncachedAudioItems.iterator();
-			while (it.hasNext() && !isCancelled()) {
-				AudioItem item = it.next();
-				try {
-					itemBeingProcessed = item.getUuid();
-					ACMConfiguration.getCurrentDB().getRepository()
-							.convert(item, AudioFormat.WAV);
-					Thread.sleep(100);
-				} catch (ConversionException e) {
-					e.printStackTrace();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-				progress++;
-				setProgress((int) ((float) progress / uncachedAudioItems.size() * 100.0));
-			}
-			itemBeingProcessed = "done";
-			return null;
-		}
+        /*
+         * Main task. Executed in background thread.
+         */
+        @Override
+        public Void doInBackground() {
+            int progress = 0;
+            // Initialize progress property.
+            setProgress(0);
+            Iterator<AudioItem> it = uncachedAudioItems.iterator();
+            while (it.hasNext() && !isCancelled()) {
+                AudioItem item = it.next();
+                try {
+                    itemBeingProcessed = item.getUuid();
+                    System.out.println("Converting " + itemBeingProcessed);
+                    ACMConfiguration.getCurrentDB().getRepository()
+                    .convert(item, AudioFormat.WAV);
+                } catch (ConversionException e) {
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                progress++;
+                setProgress((int) ((float) progress / uncachedAudioItems.size() * 100.0));
+            }
+            itemBeingProcessed = "done";
+            return null;
+        }
 
-		@Override
-		public String toString() {
-			String s = "Converting ";
-			if (itemBeingProcessed != null) {
-				s += itemBeingProcessed;
-			}
-			return s;
-		}
-	}
+        @Override
+        public String toString() {
+            String s = "Converting ";
+            if (itemBeingProcessed != null) {
+                s += itemBeingProcessed;
+            }
+            return s;
+        }
+    }
 }
