e0d4999980af1d922d35c605eb0053b7d88636f7
michael <michael@literacybridge.org>
2016-03-08 21:33:30 +0700

    Add more search tests to LuceneMetadataStoreTest

--- acm/src/test/java/org/literacybridge/acm/store/LuceneMetadataStoreTest.java
+++ acm/src/test/java/org/literacybridge/acm/store/LuceneMetadataStoreTest.java
@@ -1,14 +1,19 @@
 package org.literacybridge.acm.store;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
-import java.io.File;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import org.literacybridge.acm.api.IDataRequestResult;
+
+import com.google.common.collect.Lists;
 
 public class LuceneMetadataStoreTest {
     @Rule
@@ -16,7 +21,7 @@ public class LuceneMetadataStoreTest {
 
     @Test
     public void testAudioItems() throws Exception {
-        LuceneMetadataStore store = newStore(tmp.newFolder());
+        LuceneMetadataStore store = newStore();
 
         AudioItem a1 = store.newAudioItem("1");
         AudioItem a2 = store.newAudioItem("2");
@@ -59,7 +64,7 @@ public class LuceneMetadataStoreTest {
 
     @Test
     public void testAudioPlaylists() throws Exception {
-        LuceneMetadataStore store = newStore(tmp.newFolder());
+        LuceneMetadataStore store = newStore();
 
         AudioItem a1 = store.newAudioItem("1");
         AudioItem a2 = store.newAudioItem("2");
@@ -103,10 +108,77 @@ public class LuceneMetadataStoreTest {
 
     @Test
     public void testSearch() throws Exception {
+        LuceneMetadataStore store = newStore();
+
+        AudioItem a1 = store.newAudioItem("1");
+        AudioItem a2 = store.newAudioItem("2");
+        AudioItem a3 = store.newAudioItem("3");
+
+        a1.getMetadata().setMetadataField(MetadataSpecification.DC_TITLE, MetadataValue.newValue("Lorem"));
+        a2.getMetadata().setMetadataField(MetadataSpecification.DC_TITLE, MetadataValue.newValue("Lorem ipsum"));
+        a3.getMetadata().setMetadataField(MetadataSpecification.DC_TITLE, MetadataValue.newValue("Lorem ipsum dolor"));
+
+        assertNumSearchResults(store, "lor", 0);
+        store.commit(a1, a2, a3);
+
+        assertNumSearchResults(store, "lor", 3);
+        assertNumSearchResults(store, "lor ip ", 2);
+        assertNumSearchResults(store, "lor ip DO", 1);
+
+        Playlist p1 = store.newPlaylist("1");
+        Playlist p2 = store.newPlaylist("2");
+        Playlist p3 = store.newPlaylist("3");
+
+        p1.addAudioItem(a1);
+        p1.addAudioItem(a2);
+        p1.addAudioItem(a3);
+
+        p2.addAudioItem(a1);
+        p2.addAudioItem(a2);
+
+        p3.addAudioItem(a1);
+
+        store.commit(p1, p2, p3);
+
+        assertNumSearchResults(store, "lor ip do", p1, 1);
+        assertNumSearchResults(store, "lor ip do", p2, 0);
+        assertNumSearchResults(store, "lor ip do", p3, 0);
+        assertNumSearchResults(store, "lor", p2, 2);
+        assertNumSearchResults(store, "lor", p1, 3);
+
+        Iterator<Category> it = store.getTaxonomy().getRootCategory().getChildren().iterator();
+        Category cat1 = it.next();
+        Category cat1_1 = cat1.getChildren().iterator().next();
+        Category cat2 = it.next();
+        Category cat3 = it.next();
+        Category cat3_1 = cat3.getChildren().iterator().next();
+
+        a1.addCategory(cat3);   // this will also add the first child leaf of cat 3
+        a2.addCategory(cat1_1); // this should also automatically add the parent cat1 to this audioitem
+        a1.addCategory(cat2);
+        a2.addCategory(cat2);
+        a3.addCategory(cat2);
+        store.commit(a1, a2, a3);
+
+        assertNumSearchResults(store, "lor", Lists.newArrayList(cat3_1), null, 1);
+        assertNumSearchResults(store, "lor", Lists.newArrayList(cat2), null, 3);
+        assertNumSearchResults(store, "lor", Lists.newArrayList(cat1), null, 1);
+        assertNumSearchResults(store, "lor", Lists.newArrayList(cat1_1), null, 1);
+
+        a1.getMetadata().setMetadataField(MetadataSpecification.DC_LANGUAGE, MetadataValue.newValue(new RFC3066LanguageCode("en-us")));
+        a2.getMetadata().setMetadataField(MetadataSpecification.DC_LANGUAGE, MetadataValue.newValue(new RFC3066LanguageCode("en-us")));
+        a3.getMetadata().setMetadataField(MetadataSpecification.DC_LANGUAGE, MetadataValue.newValue(new RFC3066LanguageCode("de-de")));
+        store.commit(a1, a2, a3);
+
+        assertNumSearchResults(store, "lor", Lists.newArrayList(cat2), Lists.newArrayList(Locale.ENGLISH, Locale.GERMAN), 3);
+        assertNumSearchResults(store, "lor", Lists.newArrayList(cat2), Lists.newArrayList(Locale.ENGLISH), 2);
+        assertNumSearchResults(store, "lor", Lists.newArrayList(cat2), Lists.newArrayList(Locale.GERMAN), 1);
+        assertNumSearchResults(store, "lor do", Lists.newArrayList(cat2), Lists.newArrayList(Locale.GERMAN), 1);
+        assertNumSearchResults(store, "lor do", Lists.newArrayList(cat2), Lists.newArrayList(Locale.ENGLISH), 0);
     }
 
-    private static LuceneMetadataStore newStore(File path) throws Exception {
-        AudioItemIndex index = AudioItemIndex.newIndex(path);
+    private LuceneMetadataStore newStore() throws Exception {
+        AudioItemIndex index = AudioItemIndex.newIndex(tmp.newFolder());
         Taxonomy taxonomy = Taxonomy.createTaxonomy(null);
         return new LuceneMetadataStore(taxonomy, index);
     }
@@ -118,6 +190,12 @@ public class LuceneMetadataStoreTest {
     private static void assertNumSearchResults(MetadataStore store, String query,
             Playlist playlist, int expectedNumResults) throws Exception {
         IDataRequestResult result = store.search(query, playlist);
+        assertEquals(expectedNumResults, result.getAudioItems().size());
+    }
+
+    private static void assertNumSearchResults(MetadataStore store, String query,
+            List<Category> categories, List<Locale> locales, int expectedNumResults) throws Exception {
+        IDataRequestResult result = store.search(query, categories, locales);
         assertEquals(expectedNumResults, result.getAudioItems().size());
     }
 
