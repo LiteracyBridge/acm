ab54c4231af11e4921c10cfb1625c575ae89a3a5
bill <bill@literacybridge.org>
2016-08-22 11:45:12 -1000

    Enhanced formatting of pretty option.
    Compute fixed width columns for all the fields we're going to display, so
    the pretty output lines up nicely.

--- acmCheckOut/status.py
+++ acmCheckOut/status.py
@@ -1,3 +1,4 @@
+#!/usr/local/bin/python
 """
 status.py
 
@@ -12,21 +13,25 @@ optional arguments:  ACM_NAME   retrieve info on only this one ACM
 default: 'python status.py'     retrieve all ACMs from db
 
 """
-import boto3
+
 import argparse
+import re
 import sys
+
+import boto3
 
 # specify back-end dynamoDB table
 TABLE = 'acm_check_out'
 dynamodb = boto3.resource('dynamodb', region_name='us-west-2')
 table = dynamodb.Table(TABLE)
-PRETTY_KEYS = ['acm_name', 'acm_state', 'now_out_name', 'now_out_date', 'last_in_name', 'last_in_date']
+PRETTY_KEYS = ['acm_name', 'acm_state', 'now_out_name', 'now_out_date', 'last_in_name', 'last_in_date', 'last_in_file_name']
 PRETTY_LABELS = {'acm_name':'ACM',
                'acm_state':"Status",
                'last_in_name':'Last In By',
-               'last_in_date':"Date",
+               'last_in_date':"Checkin Date",
+               'last_in_file_name':"In File",
                'now_out_name':'Now Out By',
-               'now_out_date':'Date'
+               'now_out_date':'Checkout Date'
                }
 
 def status(acm_name=None, only_out=False, verbose=False, all=False):
@@ -45,29 +50,75 @@ def status(acm_name=None, only_out=False, verbose=False, all=False):
         # whatever the user specified, ignoring case
         name_re = re.compile('(?i)' + acm_name)
 
-    # no acm specified: query all acm entries from db
+    # Get list of acms
     acms = table.scan(TableName=TABLE).get('Items')
+    if len(acms) == 0:
+        sys.stderr.write('No ACM(s) found\n')
+        exit(3)
 
-    if not acms:    # early exit if nothing found in table
-        print('Table is empty')
-        sys.exit(2)
+    # Match by name
+    acms = [acm for acm in acms if name_re.search(acm.get('acm_name'))]
+    if len(acms) == 0:
+        sys.stderr.write('No matching ACM(s)\n')
+        exit(2)
 
-    for acm in acms:
-        if only_out and acm.get('acm_state') == "CHECKED_IN":
-            continue
-        else:
-            print_record(acm, pretty)
+    # Status, if desired
+    if only_out:
+        acms = [acm for acm in acms if acm.get('acm_state') == "CHECKED_OUT"]
+    if len(acms) == 0:
+        sys.stderr.write('No matching checked-out ACM(s)\n')
+        exit(1)
 
+    # Sort to be easier to find an ACM (not needed now, but maybe someday)
+    acms = sorted(acms, key=lambda x: x.get('acm_name'))
 
-# helper function that formats & prints records
-def print_record(acm, pretty):
-    if pretty:  # specify what parameters to retrieve
-        params = ['acm_name', 'acm_state', 'last_in_name', 'last_in_date', 'now_out_name', 'now_out_date']
-        print ','.join([param + ':' + '"' + str(acm.get(param)) + '"' for param in params])
+    print_records(acms, not verbose)
+
+# Format and print for all records
+def print_records(acms, pretty):
+    if pretty:
+        widths = get_widths(acms)
+        print_headings(widths)
+        for acm in acms:
+            print_pretty(acm, widths)
     else:
-        print ','.join(str(k) + ':' + '"' + str(v) + '"' for k, v in sorted(acm.items()))
+        for acm in acms:
+            print_simple(acm)
 
+# Simple format, all fields
+def print_simple(acm):
+    print ','.join(str(k) + ':' + '"' + str(v) + '"' for k, v in sorted(acm.items()))
 
+# Get widths for pretty-printing
+def get_widths(acms):
+    widths = {}
+    # At least as wide as the key name
+    for k in PRETTY_KEYS:
+        widths[k] = len(PRETTY_LABELS[k])
+    # And as wide as the widest value
+    for acm in acms:
+        for k in PRETTY_KEYS:
+            widths[k] = max(widths.get(k,0), len(acm.get(k, '')))
+    return widths
+
+# Print the headings for a pretty listing
+def print_headings(widths):
+    lbl = ''
+    eql = ''
+    for k in PRETTY_KEYS:
+        w = widths[k]
+        lbl += '{0:{width}} '.format(PRETTY_LABELS.get(k, ''), width=w)
+        eql += '{0:=<{width}} '.format('', width=w)
+    print(lbl)
+    print(eql)
+
+# Print a pretty line
+def print_pretty(acm, widths):
+    str = ''
+    for k in PRETTY_KEYS:
+        w = widths[k]
+        str += '{0:{width}} '.format(acm.get(k, ''), width=w)
+    print(str)
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
