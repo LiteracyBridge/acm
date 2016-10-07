cd0be9293861657f644cee10fdcf6b2eacf2baf0
bill <bill@literacybridge.org>
2016-10-07 11:08:50 -1000

    Implement a small script to easily deploy to Dropbox distribution point(s).
    Modify the build.gradle script to copy that deploy script to the 'dist' directory.

---
+++ acm/scripts/deployToDbx.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# Script to deploy the built ACM
+set -eu
+traditionalIFS="$IFS"
+IFS="`printf '\n\t'`"
+
+
+# Find dropbox.
+if [ -z ${dropbox-} ]; then
+    dropbox=$(java -cp acm.jar:lib/* org.literacybridge.acm.utils.DropboxFinder)
+    if [ $? -ne 0 ]; then
+        if [ -e ~/Dropbox\ \(Literacy\ Bridge\) ]; then
+            dropbox=~/Dropbox\ \(Literacy\ Bridge\)
+        elif [ -e ~/Dropbox ]; then
+            dropbox=~/Dropbox
+        else
+            echo "Can't find Dropbox."
+            exit 100
+        fi
+    fi
+    export dropbox=$dropbox
+fi
+echo "Dropbox is in $dropbox"
+
+# Convenience shortcuts.
+installDir=$dropbox/LB-software/ACM-install
+acmDir=$installDir/ACM/software
+
+# Add any missing libs
+for f in $(ls lib); do
+    if [ ! -e ${acmDir}/lib/${f} ]; then
+        echo cp lib/${f} ${acmDir}/lib/${f} 
+        cp lib/${f} ${acmDir}/lib/${f} 
+    fi
+done
+
+# Update the acm.jar. 
+# TODO: Make it exist in one place only
+for f in $(find ${dropbox} -iname acm.jar); do
+    echo cp acm.jar $f
+    cp acm.jar $f
+done
+
+# Remove any obsolete libs
+for f in $(ls ${acmDir}/lib); do
+    if [ ! -e lib/${f} ]; then
+        echo rm ${acmDir}/lib/${f}
+        # rm ${acmDir}/lib/${f}
+    fi
+done
+
+echo "Be sure to update $(ls ${installDir}/*.rev)"
+
