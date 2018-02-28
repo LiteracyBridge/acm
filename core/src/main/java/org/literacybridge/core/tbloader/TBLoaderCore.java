LiteracyBridge/acm

commit 5ad2aed582e9fb7dcc0f8c5842f6ffb7219092b8

Write recipientid to Talking Book.

core/src/main/java/org/literacybridge/core/tbloader/TBLoaderCore.java
@@ -501,6 +501,10 @@ public class TBLoaderCore {
                     .put("timestamp", mUpdateTimestampISO)
                     .put("duration", Integer.toString(durationSeconds))
                     .put("testing", mNewDeploymentInfo.isTestDeployment());
+                if (mNewDeploymentInfo.getRecipientid() != null) {
+                    opLog.put("out_recipientid", mNewDeploymentInfo.getRecipientid());
+                    deploymentLog.put("recipientid", mNewDeploymentInfo.getRecipientid());
+                }
                 if (mCoordinates != null && mCoordinates.length() > 0) {
                     deploymentLog.put("coordinates", mCoordinates);
                 }
@@ -518,6 +522,10 @@ public class TBLoaderCore {
                 .put("in_synchdir", (lastSynchDir != null ? lastSynchDir.toUpperCase() : ""))
                 .put("in_disk_label", mTbDeviceInfo.getLabel())
                 .put("disk_corrupted", mTbDeviceInfo.isCorrupted());
+            if (mOldDeploymentInfo.getRecipientid() != null) {
+                statsInfo.put("in_recipientid", mOldDeploymentInfo.getRecipientid());
+                opLog.put("in_recipientid", mOldDeploymentInfo.getRecipientid());
+            }
 
             if (mTtbFlashData != null) {
                 bw.write(mTtbFlashData.getSerialNumber().toUpperCase() + ",");
@@ -1400,6 +1408,9 @@ public class TBLoaderCore {
         if (mCoordinates != null && mCoordinates.length() > 0) {
             props.append(TBLoaderConstants.COORDINATES_PROPERTY, mCoordinates);
         }
+        if (mNewDeploymentInfo.getRecipientid() != null) {
+            props.append(TBLoaderConstants.RECIPIENTID_PROPERTY, mNewDeploymentInfo.getRecipientid());
+        }
         eraseAndOverwriteFile(system.open(TBLoaderConstants.DEPLOYMENT_PROPERTIES_NAME), props.toString());
 
         finishStep();
@@ -1442,11 +1453,20 @@ public class TBLoaderCore {
      */
     private void updateCommunity() throws IOException {
         startStep(updateCommunity);
+        // Keep 10.a18 and foo.grp files; skip recipient.id
+        TbFile.CopyFilter filter = new TbFile.CopyFilter() {
+            @Override
+            public boolean accept(TbFile file) {
+                String name = file.getName().toLowerCase();
+                return name.endsWith(".a18") || name.endsWith("*.grp");
+            }
+        };
 
         TbFile communityPath = mDeploymentDirectory.open(TBLoaderConstants.COMMUNITIES_SUBDIR).open(
                 mNewDeploymentInfo.getCommunity());
         if (communityPath.exists()) {
-            TbFile.copyDir(communityPath, mTalkingBookRoot, null, mCopyListener);
+            TbFile.copyDir(communityPath,
+                mTalkingBookRoot, filter, mCopyListener);
         }
 
         finishStep();

acm/src/main/java/org/literacybridge/acm/tbloader/TBLoader.java
@@ -12,12 +12,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -666,6 +669,15 @@ public class TBLoader extends JFrame {
         newCommunityModel.setFilterString(filter);
     }
 
+    private String getRecipientIdForCommunity(String communityDirName) {
+        // ~/LiteracyBridge/TB-Loaders/{project}/content/{deployment}/communities/{communitydir}
+        File deploymentDirectory = new File(baseDirectory,
+            TBLoaderConstants.CONTENT_SUBDIR + File.separator
+                + newDeploymentList.getSelectedItem().toString());
+
+        return TBLoaderUtils.getRecipientIdForCommunity(deploymentDirectory, communityDirName);
+    }
+
     private synchronized void setCommunityList() throws IOException {
         if (prevSelectedCommunity != -1)
             newCommunityList.setSelectedIndex(prevSelectedCommunity);
@@ -1304,7 +1316,22 @@ public class TBLoader extends JFrame {
             }
         }
 
-        private void update(DeploymentInfo newDeploymentInfo) {
+        private void update() {
+            String community = newCommunityList.getSelectedItem().toString();
+            DeploymentInfo.DeploymentInfoBuilder builder = new DeploymentInfo.DeploymentInfoBuilder()
+                .withSerialNumber(newSrnText.getText())
+                .withNewSerialNumber(isNewSerialNumber)
+                .withProjectName(newProject)
+                .withDeploymentName(newDeploymentList.getSelectedItem().toString())
+                .withPackageName(newImageText.getText())
+                .withUpdateDirectory(null)
+                .withUpdateTimestamp(dateRotation)
+                .withFirmwareRevision(newFirmwareVersionText.getText())
+                .withCommunity(community)
+                .withRecipientid(getRecipientIdForCommunity(community))
+                .asTestDeployment(testDeployment.isSelected());
+            DeploymentInfo newDeploymentInfo = builder.build();
+
             String endMsg = "";
             String endTitle = "";
             OperationLog.Operation opLog = OperationLog.startOperation("TbLoaderUpdate");
@@ -1401,20 +1428,8 @@ public class TBLoader extends JFrame {
 
         @Override
         public void run() {
-            DeploymentInfo.DeploymentInfoBuilder builder = new DeploymentInfo.DeploymentInfoBuilder()
-                    .withSerialNumber(newSrnText.getText())
-                    .withNewSerialNumber(isNewSerialNumber)
-                    .withProjectName(newProject)
-                    .withDeploymentName(newDeploymentList.getSelectedItem().toString())
-                    .withPackageName(newImageText.getText())
-                    .withUpdateDirectory(null)
-                    .withUpdateTimestamp(dateRotation)
-                    .withFirmwareRevision(newFirmwareVersionText.getText())
-                    .withCommunity(newCommunityList.getSelectedItem().toString())
-                    .asTestDeployment(testDeployment.isSelected());
-            DeploymentInfo newDeploymentInfo = builder.build();
             if (this.operation == Operation.Update) {
-                update(newDeploymentInfo);
+                update();
             } else if (this.operation == Operation.CollectStats) {
                 grabStatsOnly();
             }

androidTBLoader/src/main/java/org/literacybridge/androidtbloader/content/ContentInfo.java
@@ -6,8 +6,12 @@ import org.literacybridge.androidtbloader.checkin.KnownLocations;
 import org.literacybridge.androidtbloader.community.CommunityInfo;
 import org.literacybridge.androidtbloader.util.PathsProvider;
 import org.literacybridge.core.fs.OperationLog;
+import org.literacybridge.core.tbloader.TBLoaderConstants;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;

androidTBLoader/src/main/java/org/literacybridge/androidtbloader/tbloader/TbLoaderFragment.java
@@ -43,7 +43,9 @@ import org.literacybridge.core.tbloader.TBLoaderConfig;
 import org.literacybridge.core.tbloader.TBLoaderCore;
 import org.literacybridge.core.tbloader.TBLoaderUtils;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
@@ -738,6 +740,7 @@ public class TbLoaderFragment extends Fragment {
         if (tbDeviceInfo.needNewSerialNumber()) {
             deviceSerialNumber = getNewDeviceSerialNumber();
         }
+        String recipientid = TBLoaderUtils.getRecipientIdForCommunity(deploymentDirectory, mCommunity.getName());
 
         DeploymentInfo.DeploymentInfoBuilder builder = new DeploymentInfo.DeploymentInfoBuilder()
                 .withSerialNumber(deviceSerialNumber)
@@ -749,6 +752,7 @@ public class TbLoaderFragment extends Fragment {
                 .withUpdateTimestamp(todaysDate) // TODO: this should be the "Deployment date", the first date the new content is deployed.
                 .withFirmwareRevision(firmwareRevision)
                 .withCommunity(mCommunity.getName())
+                .withRecipientid(recipientid)
                 .asTestDeployment(mTestDeploymentCheckBox.isChecked());
         DeploymentInfo newDeploymentInfo = builder.build();
 

core/src/main/java/org/literacybridge/core/fs/TbFile.java
@@ -149,8 +149,8 @@ public abstract class TbFile {
         void copying(String filename);
     }
 
-    public abstract static class CopyFilter {
-        public abstract boolean accept(TbFile file);
+    public interface CopyFilter {
+        boolean accept(TbFile file);
     }
 
     public static long copy(TbFile src, TbFile dst) throws IOException {

core/src/main/java/org/literacybridge/core/tbloader/DeploymentInfo.java
@@ -30,6 +30,8 @@ public class DeploymentInfo {
     private final String firmwareRevision;
     // The name of the community in which the Talking Book was last deployed.
     private final String community;
+    // The recipientid of the community/group of the deployment.
+    private final String recipientid;
 
     // Was (or will) the deployment for testing purposes.
     private final boolean testDeployment;
@@ -44,6 +46,7 @@ public class DeploymentInfo {
         private String updateTimestamp = null;
         private String firmwareRevision = UNKNOWN;
         private String community = UNKNOWN;
+        private String recipientid = null;
         private boolean testDeployment = false;
 
         public DeploymentInfoBuilder withSerialNumber(String serialNumber) {
@@ -91,6 +94,11 @@ public class DeploymentInfo {
             return this;
         }
 
+        public DeploymentInfoBuilder withRecipientid(String recipientid) {
+            this.recipientid = recipientid;
+            return this;
+        }
+
         public DeploymentInfoBuilder asTestDeployment(boolean testDeployment) {
             this.testDeployment = testDeployment;
             return this;
@@ -107,6 +115,7 @@ public class DeploymentInfo {
             updateTimestamp = di.updateTimestamp;
             firmwareRevision = di.firmwareRevision;
             community = di.community;
+            recipientid = di.recipientid;
             testDeployment = di.testDeployment;
             return this;
         }
@@ -133,6 +142,7 @@ public class DeploymentInfo {
         this.updateTimestamp = builder.updateTimestamp;
         this.firmwareRevision = builder.firmwareRevision;
         this.community = builder.community;
+        this.recipientid = builder.recipientid;
         this.testDeployment = builder.testDeployment;
     }
     
@@ -172,6 +182,10 @@ public class DeploymentInfo {
         return community;
     }
 
+    public String getRecipientid() {
+        return recipientid;
+    }
+
     public boolean isTestDeployment() { return testDeployment; }
 
     @Override
@@ -184,6 +198,7 @@ public class DeploymentInfo {
                 + "\nupdated timestamp: " + updateTimestamp
                 + "\nFirmware revision: " + firmwareRevision
                 + "\nCommunity: " + community
+                + ( (recipientid != null) ? ("\nRecipientid: "+recipientid) : ("") )
                 + "\nTest: " + (Boolean)testDeployment;
     }
 }

core/src/main/java/org/literacybridge/core/tbloader/TBDeviceInfo.java
@@ -11,13 +11,22 @@ import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import static org.literacybridge.core.tbloader.TBLoaderConstants.BINARY_STATS_ALTERNATIVE_PATH;
+import static org.literacybridge.core.tbloader.TBLoaderConstants.BINARY_STATS_PATH;
 import static org.literacybridge.core.tbloader.TBLoaderConstants.COMMUNITY_PROPERTY;
 import static org.literacybridge.core.tbloader.TBLoaderConstants.DEPLOYMENT_PROPERTIES_NAME;
 import static org.literacybridge.core.tbloader.TBLoaderConstants.DEPLOYMENT_PROPERTY;
+import static org.literacybridge.core.tbloader.TBLoaderConstants.NEED_SERIAL_NUMBER;
+import static org.literacybridge.core.tbloader.TBLoaderConstants.NO_DRIVE;
 import static org.literacybridge.core.tbloader.TBLoaderConstants.PACKAGE_PROPERTY;
 import static org.literacybridge.core.tbloader.TBLoaderConstants.PROJECT_FILE_EXTENSION;
 import static org.literacybridge.core.tbloader.TBLoaderConstants.PROJECT_PROPERTY;
+import static org.literacybridge.core.tbloader.TBLoaderConstants.RECIPIENTID_PROPERTY;
 import static org.literacybridge.core.tbloader.TBLoaderConstants.TALKING_BOOK_ID_PROPERTY;
+import static org.literacybridge.core.tbloader.TBLoaderConstants.TB_AUDIO_PATH;
+import static org.literacybridge.core.tbloader.TBLoaderConstants.TB_LANGUAGES_PATH;
+import static org.literacybridge.core.tbloader.TBLoaderConstants.TB_LISTS_PATH;
+import static org.literacybridge.core.tbloader.TBLoaderConstants.TB_SYSTEM_PATH;
 import static org.literacybridge.core.tbloader.TBLoaderConstants.TEST_DEPLOYMENT_PROPERTY;
 import static org.literacybridge.core.tbloader.TBLoaderConstants.UNKNOWN;
 import static org.literacybridge.core.tbloader.TBLoaderUtils.isSerialNumberFormatGood;
@@ -47,7 +56,7 @@ public final class TBDeviceInfo {
 
 
     public static TBDeviceInfo getNullDeviceInfo() {
-        return new TBDeviceInfo(TBLoaderConstants.NO_DRIVE, null);
+        return new TBDeviceInfo(NO_DRIVE, null);
     }
 
     public TBDeviceInfo(TbFile tbRoot, String label, String prefix) {
@@ -57,7 +66,7 @@ public final class TBDeviceInfo {
         this.tbRoot = tbRoot;
         this.label = label.trim();
         tbPrefix = prefix;
-        tbSystem = tbRoot.open(TBLoaderConstants.TB_SYSTEM_PATH);
+        tbSystem = tbRoot.open(TB_SYSTEM_PATH);
         tbFlashData = loadFlashData(tbRoot);
         loadDeploymentProperties();
         loadSerialNumber();
@@ -122,10 +131,10 @@ public final class TBDeviceInfo {
             return false;
         }
 
-        return tbRoot.open(TBLoaderConstants.TB_SYSTEM_PATH).exists()
-                && tbRoot.open(TBLoaderConstants.TB_LANGUAGES_PATH).exists()
-                && tbRoot.open(TBLoaderConstants.TB_LISTS_PATH).exists()
-                && tbRoot.open(TBLoaderConstants.TB_AUDIO_PATH).exists();
+        return tbRoot.open(TB_SYSTEM_PATH).exists()
+                && tbRoot.open(TB_LANGUAGES_PATH).exists()
+                && tbRoot.open(TB_LISTS_PATH).exists()
+                && tbRoot.open(TB_AUDIO_PATH).exists();
     }
 
     /**
@@ -135,11 +144,11 @@ public final class TBDeviceInfo {
     private static TbFlashData loadFlashData(TbFile tbRoot) {
         TbFlashData tbFlashData = null;
         try {
-            TbFile flashDataBin = tbRoot.open(TBLoaderConstants.BINARY_STATS_PATH);
+            TbFile flashDataBin = tbRoot.open(BINARY_STATS_PATH);
             if (flashDataBin.exists()) {
                 tbFlashData = new TbFlashData(flashDataBin);
             } else {
-                flashDataBin = tbRoot.open(TBLoaderConstants.BINARY_STATS_ALTERNATIVE_PATH);
+                flashDataBin = tbRoot.open(BINARY_STATS_ALTERNATIVE_PATH);
                 if (flashDataBin.exists()) {
                     tbFlashData = new TbFlashData(flashDataBin);
                 }
@@ -164,7 +173,7 @@ public final class TBDeviceInfo {
                 !TBLoaderUtils.isSerialNumberFormatGood2(serialNumber)) {
             needNewSerialNumber = true;
             // We will allocate a new-style serial number before we update the tbDeviceInfo.
-            serialNumber = TBLoaderConstants.NEED_SERIAL_NUMBER;
+            serialNumber = NEED_SERIAL_NUMBER;
         }
     }
 
@@ -227,7 +236,7 @@ public final class TBDeviceInfo {
         if (tbRoot == null) {
             return sn;
         }
-        TbFile tbSystem = tbRoot.open(TBLoaderConstants.TB_SYSTEM_PATH);
+        TbFile tbSystem = tbRoot.open(TB_SYSTEM_PATH);
         Properties props = loadDeploymentProperties(tbSystem);
 
         sn = props.getProperty(TALKING_BOOK_ID_PROPERTY, UNKNOWN);
@@ -512,6 +521,24 @@ public final class TBDeviceInfo {
         return communityName;
     }
 
+    /**
+     * Reads the recipient id from the deployment properties file. There is no other location
+     * for this datum.
+     * @return The recipient id, or null if it can not be determines.
+     */
+    public String getRecipientid() {
+        String recipientid = null;
+        Properties properties = loadDeploymentProperties();
+        if (properties != null) {
+            recipientid = properties.getProperty(RECIPIENTID_PROPERTY, null);
+            LOG.log(Level.INFO, String.format("TBL!: recipientid: %s", recipientid));
+        } else {
+            LOG.log(Level.INFO, String.format("TBL!: recipientid: (null) (no deployment.properties)"));
+        }
+
+        return recipientid;
+    }
+
     public boolean isTestDeployment() {
         if (testDeployment == null) {
             testDeployment = false;
@@ -584,6 +611,7 @@ public final class TBDeviceInfo {
 
         try {
             String  community = getCommunityName();
+            String recipientid = getRecipientid();
 
             String firmwareVersion = getFirmwareVersion();
 
@@ -610,6 +638,7 @@ public final class TBDeviceInfo {
                     .withUpdateTimestamp(lastUpdate)
                     .withFirmwareRevision(firmwareVersion)
                     .withCommunity(community)
+                    .withRecipientid(recipientid)
                     .withFallbackProjectName(fallbackProjectName)
                     .asTestDeployment(isTestDeployment());
             deploymentInfo = builder.build();

core/src/main/java/org/literacybridge/core/tbloader/TBLoaderConstants.java
@@ -56,6 +56,7 @@ public class TBLoaderConstants {
     public static final String DEPLOYMENT_PROPERTY = "DEPLOYMENT";
     public static final String PACKAGE_PROPERTY = "PACKAGE";
     public static final String COMMUNITY_PROPERTY = "COMMUNITY";
+    public static final String RECIPIENTID_PROPERTY = "RECIPIENTID";
     public static final String TALKING_BOOK_ID_PROPERTY = "TALKINGBOOKID";
     public static final String TIMESTAMP_PROPERTY = "TIMESTAMP";
     public static final String TEST_DEPLOYMENT_PROPERTY = "TESTDEPLOYMENT";

commit 9f7028ec26db91c6aaff38eba2d03f43317b0b4b

Fix bug deploying custom greetings.
The change to add recipient.id to communitiy directories introduced a
bug that failed to copy custom greetings to Talking Books. This fixes
that bug. (Bug introduced in 5ad2aed58.)

core/src/main/java/org/literacybridge/core/tbloader/TBLoaderCore.java
@@ -1458,6 +1458,7 @@ public class TBLoaderCore {
         TbFile.CopyFilter filter = new TbFile.CopyFilter() {
             @Override
             public boolean accept(TbFile file) {
+                if (file.isDirectory()) return true;
                 String name = file.getName().toLowerCase();
                 return name.endsWith(".a18") || name.endsWith("*.grp");
             }