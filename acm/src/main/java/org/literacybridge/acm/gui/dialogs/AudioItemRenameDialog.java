cbab84696eea5b82bb3161efc883382ba45bdc25
bill <bill@literacybridge.org>
2019-11-08 14:56:35 -0800

    Fix bug in "rename message" when there is no Program Specification.

--- acm/src/main/java/org/literacybridge/acm/gui/dialogs/AudioItemRenameDialog.java
+++ acm/src/main/java/org/literacybridge/acm/gui/dialogs/AudioItemRenameDialog.java
@@ -826,7 +826,7 @@ public class AudioItemRenameDialog extends JDialog {
                 transaction.add(item);
             });
         }
-        if (addToPlaylist.isSelected()) {
+        if (addToPlaylist != null && addToPlaylist.isSelected()) {
             getPlaylistsForItemWithTitle(toRename, newTitle)
                 .forEach(triple -> {
                     AudioItem item = triple.getLeft();
