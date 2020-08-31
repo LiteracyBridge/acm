aed412aed97b1a771c18937203832d9b941ba4e6
Bill Evans <billev2k@gmail.com>
2020-08-31 10:48:24 -0700

    Fix a bug with variants when no actual variant content.
    - The bug prevented the non-variant package being created, but sometimes
      that non-variant package is the one needed.

--- core/src/main/java/org/literacybridge/core/spec/ProgramSpec.java
+++ core/src/main/java/org/literacybridge/core/spec/ProgramSpec.java
@@ -162,6 +162,7 @@ public class ProgramSpec {
     public Set<String> getVariantsForDeploymentAndLanguage(int deploymentNumber, String languagecode) {
         RecipientList recipients = getRecipientsForDeploymentAndLanguage(deploymentNumber, languagecode);
         Set<String> recipientVariants = recipients.stream().map(r -> r.variant).collect(Collectors.toSet());
+        recipientVariants.add("");
         // Find all of the message variants for that language in the deployment.
         Set<String> messageVariants = new HashSet<>();
         messageVariants.add("");
