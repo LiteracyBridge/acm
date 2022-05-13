45fa588dddcb6eb3f5b06145b45640a050150dc8
Bill Evans <billev2k@gmail.com>
2022-05-13 09:18:42 -1000

    Accept more formats for system prompt filenames.
    - We've seen system prompt filenames like "10 welcome". This change looks for
      ID-like strings followed by description-like strings, and when that's found,
      matches the id part and description part separately.

--- acm/src/main/java/org/literacybridge/acm/gui/assistants/PromptsImport/PromptsMatcher.java
+++ acm/src/main/java/org/literacybridge/acm/gui/assistants/PromptsImport/PromptsMatcher.java
@@ -2,12 +2,18 @@ package org.literacybridge.acm.gui.assistants.PromptsImport;
 
 import me.xdrop.fuzzywuzzy.FuzzySearch;
 import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.literacybridge.acm.gui.assistants.Matcher.ImportableFile;
 import org.literacybridge.acm.gui.assistants.Matcher.Matcher;
 
 import java.util.function.BiFunction;
+import java.util.regex.Pattern;
 
 public class PromptsMatcher extends Matcher<PromptTarget, ImportableFile, PromptMatchable> {
+    // Some programs have named the prompts with both the ID and the descriptive name, like
+    // "10 welcome" or "i9-0 user feedback invitation". This recognizes most such formatting
+    // and splits apart the ID and descriptive name, which are then matched independently.
+    static final Pattern promptPattern = Pattern.compile("(?i)^((?:i?\\W*)?\\d+(?:-\\d+)?)\\s*(.+)$");
 
     @Override
     protected int scoreMatch(PromptMatchable l, PromptMatchable r, boolean tokens) {
@@ -20,22 +26,28 @@ public class PromptsMatcher extends Matcher<PromptTarget, ImportableFile, Prompt
                                                  FuzzySearch::ratio;
 
         String fileName = FilenameUtils.removeExtension(file.getFile().getName());
+        String idPart = null;
+        String descriptivePart = null;
+        java.util.regex.Matcher matcher = promptPattern.matcher(fileName);
+        if (matcher.matches()) {
+            idPart = matcher.group(1);
+            descriptivePart = matcher.group(2);
+        }
 
-        // Try to match with both the prompt id and definition.
-        int score = fn.apply(prompt.getPromptId(), fileName);
-        score = Math.max(score, fn.apply(prompt.getPromptFilename(), fileName));
-        score = Math.max(score, fn.apply(prompt.getPromptText(), fileName));
-
-        if (tokens) {
-            int t1 = FuzzySearch.weightedRatio(prompt.getPromptText(), fileName);
-            int t2 = FuzzySearch.weightedRatio(prompt.getPromptFilename(), fileName);
-            int t3 = FuzzySearch.weightedRatio(prompt.getPromptId(), fileName);
-            if (t1 > 86) {
-                System.out.printf("%d, %s :: %s\n", t1, fileName, prompt.getPromptText());
+        int score = 0;
+        try {
+            // Try to match with both the prompt id and definition.
+            score = fn.apply(prompt.getPromptId(), fileName);
+            score = Math.max(score, fn.apply(prompt.getPromptFilename(), fileName));
+            score = Math.max(score, fn.apply(prompt.getPromptText(), fileName));
+            if (StringUtils.isNotEmpty(idPart)) {
+                score = Math.max(score, fn.apply(prompt.getPromptId(), idPart));
             }
-            if (t3 > 90) {
-                System.out.printf("%d, %s :: %s\n", t3, fileName, prompt.getPromptId());
+            if (StringUtils.isNotEmpty(descriptivePart)) {
+                score = Math.max(score, fn.apply(prompt.getPromptFilename(), descriptivePart));
             }
+        } catch (Exception ignored) {
+            // Ignore and take whatever we got.
         }
 
         return score;
