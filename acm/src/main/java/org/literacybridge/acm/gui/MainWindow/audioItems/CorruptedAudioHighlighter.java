0f809736bd1f88b3f74fbc054a706d8a4331d5f7
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2023-11-09 14:59:03 +0000

    Little house cleaning

--- acm/src/main/java/org/literacybridge/acm/gui/MainWindow/audioItems/CorruptedAudioHighlighter.java
+++ acm/src/main/java/org/literacybridge/acm/gui/MainWindow/audioItems/CorruptedAudioHighlighter.java
@@ -8,40 +8,16 @@ import org.literacybridge.acm.gui.util.AudioItemNode;
 import java.awt.*;
 
 public class CorruptedAudioHighlighter extends AbstractHighlighter {
-    private int durationColumnIndex; // The column index containing the duration
-    private Color highlightColor;    // The color for highlighting
-
-    public CorruptedAudioHighlighter(int durationColumnIndex, Color highlightColor) {
-        this.durationColumnIndex = durationColumnIndex;
-        this.highlightColor = highlightColor;
-    }
-
     @Override
     protected Component doHighlight(Component component, ComponentAdapter adapter) {
         AudioItemNode<?> audioItemNode = (AudioItemNode<?>) adapter.getValue();
-
         String duration = audioItemNode.getAudioItem().getDuration().trim();
-//            ^(-\d+:-?\d+)|(00:00).*
-        Boolean isCorrupted = duration.trim().indexOf('-') != -1 || duration.trim().equals("00:000");
-        System.out.println(isCorrupted);
 
-        if (isCorrupted) {
-//            if (Pattern.compile("^(-\\d+:-?\\d+.*)|(00:00.*)").matcher(duration).matches()) {
+        // Audio is considered as corrupted if it has a negative or 00:00 duration
+        if (duration.trim().indexOf('-') != -1 || duration.trim().equals("00:000")) {
             component.setForeground(Color.red);
         }
-//        if (adapter.column == durationColumnIndex) {
-//            Object value = adapter.getValue(durationColumnIndex);
-//            System.out.println(value);
-//            if (value != null) {
-//                String duration = value.toString();
-//                if (duration.equals("00:00") || duration.startsWith("-")) {
-//                    component.setBackground(highlightColor);
-//                }
-////                else {
-////                    component.setBackground(Color.WHITE); // Set the default background color
-////                }
-//            }
-//        }
+
         return component;
     }
 }
