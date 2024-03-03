18cfc024fc607f19d64f38d1fd348de3d55d46d9
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2024-03-03 20:07:19 +0000

    feat(android): update home screen ui

--- TalkingBookApp/app/src/main/java/org/literacybridge/talkingbookapp/ui/HomeScreen.kt
+++ TalkingBookApp/app/src/main/java/org/literacybridge/talkingbookapp/ui/HomeScreen.kt
@@ -1,32 +1,98 @@
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import org.literacybridge.talkingbookapp.R
 
 @Composable
 fun HomeScreen(navController: NavController) {
     // Retrieve data from next screen
     val msg =
         navController.currentBackStackEntry?.savedStateHandle?.get<String>("msg")
+
+
     Column(
         Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+//        Modifier.fillMaxHeight(),
+        verticalArrangement = Arrangement.SpaceBetween
+//        horizontalAlignment = Alignment.CenterHorizontally,
+//        verticalArrangement = Arrangement.Center
     ) {
-        Button(onClick = { navController.navigate("secondscreen") }) {
-            Text("Go to next screen")
+        Column(
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+                .weight(1f, false)
+        ) {
+            Text("Connect the Talking Book")
+            Row {
+                Text("Instructions", style = TextStyle(fontWeight = FontWeight.Bold))
+                Text("Press 'tree', 'table' & “plus” buttons at the same time to connect")
+            }
+            Image(
+                painter = painterResource(R.drawable.tb_table_image),
+                contentDescription = "Tree"
+//                contentDescription = stringResource(id = R.string.bus_content_description)
+            )
+            Box(
+                modifier = Modifier
+                    .border(width = 2.dp, color = Color.Blue)
+                    .align(Alignment.CenterHorizontally)
+            ) {
+                Text(
+                    "[Display TB info once connected]\n" +
+                            "TB ID\n" +
+                            "Current recipient\n" +
+                            "Current content deployment\n"
+                )
+
+            }
         }
-        Spacer(modifier = Modifier.height(8.dp))
-        msg?.let {
-            Text(it)
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("What would you like to do?", modifier = Modifier.padding(vertical = 4.dp))
+            Row(
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Button(onClick = { /*TODO*/ }) {
+                    Text("Collect Data")
+                }
+                Button(onClick = { /*TODO*/ }) {
+                    Text("Update Talking Book")
+                }
+            }
         }
+//        Button(onClick = { navController.navigate("secondscreen") }) {
+//            Text("Go to next screen")
+//        }
+//        Spacer(modifier = Modifier.height(8.dp))
+//        msg?.let {
+//            Text(it)
+//        }
     }
+
+//    return ProgramsDropdown();
 }
---
+++ TalkingBookApp/app/src/main/java/org/literacybridge/talkingbookapp/ui/components/ProgramsDropdown.kt
@@ -0,0 +1,79 @@
+package org.literacybridge.talkingbookapp.ui.components
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.vector.ImageVector
+
+@Composable
+fun ProgramsDropdown() {
+    // ...
+    val openAlertDialog = remember { mutableStateOf(true) }
+
+    // ...
+    when {
+        // ...
+        openAlertDialog.value -> {
+            AlertDialogExample(
+                onDismissRequest = { openAlertDialog.value = false },
+                onConfirmation = {
+                    openAlertDialog.value = false
+                    println("Confirmation registered") // Add logic here to handle confirmation.
+                },
+                dialogTitle = "Alert dialog example",
+                dialogText = "This is an example of an alert dialog with buttons.",
+                icon = Icons.Default.Info
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AlertDialogExample(
+    onDismissRequest: () -> Unit,
+    onConfirmation: () -> Unit,
+    dialogTitle: String,
+    dialogText: String,
+    icon: ImageVector,
+) {
+    AlertDialog(
+        icon = {
+            Icon(icon, contentDescription = "Example Icon")
+        },
+        title = {
+            Text(text = dialogTitle)
+        },
+        text = {
+            Text(text = dialogText)
+        },
+        onDismissRequest = {
+            onDismissRequest()
+        },
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    onConfirmation()
+                }
+            ) {
+                Text("Confirm")
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = {
+                    onDismissRequest()
+                }
+            ) {
+                Text("Dismiss")
+            }
+        }
+    )
+}
