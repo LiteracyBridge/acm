df769dde15922487084265562de668b9ecdb8f95
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2024-03-13 20:55:06 +0000

    ci: add 'core' module to gradle build

--- TalkingBookApp/settings.gradle.kts
+++ TalkingBookApp/settings.gradle.kts
@@ -6,13 +6,14 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
 rootProject.name = "Talking Book App"
 include(":app")
- 
+includeFlat("core")
