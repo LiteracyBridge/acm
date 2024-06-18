045506914fae513523c893877538c8715aa6a68b
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2024-06-18 07:02:16 +0000

    fix(android: logout): app crash when navigating to login screen in Amplify.signOut result

--- TalkingBookApp/app/src/main/java/org/literacybridge/talkingbookapp/ui/LogoutUser.kt
+++ TalkingBookApp/app/src/main/java/org/literacybridge/talkingbookapp/ui/LogoutUser.kt
@@ -2,8 +2,19 @@ package org.literacybridge.talkingbookapp.ui
 
 import Screen
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.NavOptions
 import com.amplifyframework.auth.cognito.result.AWSCognitoAuthSignOutResult
 import com.amplifyframework.core.Amplify
 import io.sentry.Sentry.captureException
@@ -13,39 +24,57 @@ import io.sentry.Sentry.captureException
 fun LogoutUser(
     navController: NavController
 ) {
-    Log.d("Logout-user", "User is being logged out")
-    Amplify.Auth.signOut { signOutResult ->
-        when (signOutResult) {
-            is AWSCognitoAuthSignOutResult.CompleteSignOut -> {
-                // Sign Out completed fully and without errors.
-                Log.i("Logout-user", "Signed out successfully")
-                navController.navigate(Screen.LOGIN.name)
-            }
+    val isSuccessful = remember { mutableStateOf(false) }
 
-            is AWSCognitoAuthSignOutResult.PartialSignOut -> {
-                // Sign Out completed with some errors. User is signed out of the device.
-                signOutResult.hostedUIError?.let {
-                    // Optional: Re-launch it.url in a Custom tab to clear Cognito web session.
-                    Log.e("Logout-user", "HostedUI Error", it.exception)
-                    captureException(it.exception)
-                }
-                signOutResult.globalSignOutError?.let {
-                    // Optional: Use escape hatch to retry revocation of it.accessToken.
-                    Log.e("Logout-user", "GlobalSignOut Error", it.exception)
-                    captureException(it.exception)
-                }
-                signOutResult.revokeTokenError?.let {
-                    // Optional: Use escape hatch to retry revocation of it.refreshToken.
-                    Log.e("Logout-user", "RevokeToken Error", it.exception)
-                    captureException(it.exception)
-                }
-            }
+    if (isSuccessful.value) {
+        navController.navigate(Screen.LOGIN.name){
+            popUpTo(0)
+        }
+    }
 
-            is AWSCognitoAuthSignOutResult.FailedSignOut -> {
-                // Sign Out failed with an exception, leaving the user signed in.
-                Log.e("Logout-user", "Sign out Failed", signOutResult.exception)
-                captureException(signOutResult.exception)
+    LaunchedEffect("logout-user") {
+        Amplify.Auth.signOut { signOutResult ->
+            when (signOutResult) {
+                is AWSCognitoAuthSignOutResult.CompleteSignOut -> {
+                    // Sign Out completed fully and without errors.
+                    Log.i("Logout-user", "Signed out successfully")
+                    isSuccessful.value = true
+                }
+
+                is AWSCognitoAuthSignOutResult.PartialSignOut -> {
+                    // Sign Out completed with some errors. User is signed out of the device.
+                    signOutResult.hostedUIError?.let {
+                        // Optional: Re-launch it.url in a Custom tab to clear Cognito web session.
+                        Log.e("Logout-user", "HostedUI Error", it.exception)
+                        captureException(it.exception)
+                    }
+                    signOutResult.globalSignOutError?.let {
+                        // Optional: Use escape hatch to retry revocation of it.accessToken.
+                        Log.e("Logout-user", "GlobalSignOut Error", it.exception)
+                        captureException(it.exception)
+                    }
+                    signOutResult.revokeTokenError?.let {
+                        // Optional: Use escape hatch to retry revocation of it.refreshToken.
+                        Log.e("Logout-user", "RevokeToken Error", it.exception)
+                        captureException(it.exception)
+                    }
+                }
+
+                is AWSCognitoAuthSignOutResult.FailedSignOut -> {
+                    // Sign Out failed with an exception, leaving the user signed in.
+                    Log.e("Logout-user", "Sign out Failed", signOutResult.exception)
+                    captureException(signOutResult.exception)
+                }
             }
         }
+    }
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator(
+            modifier = Modifier.width(100.dp),
+        )
     }
 }
