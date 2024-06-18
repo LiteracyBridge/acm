25f26f48f09685d603a1958413d822e21339d9b9
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2024-06-18 05:56:56 +0000

    refactor(android: recipient): update content verification UI

--- TalkingBookApp/app/src/main/java/org/literacybridge/talkingbookapp/ui/talkingbook_update/ContentVerificationScreen.kt
+++ TalkingBookApp/app/src/main/java/org/literacybridge/talkingbookapp/ui/talkingbook_update/ContentVerificationScreen.kt
@@ -13,8 +13,6 @@ import androidx.compose.material3.Divider
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.SpanStyle
@@ -37,8 +35,6 @@ fun ContentVerificationScreen(
     viewModel: TalkingBookViewModel = viewModel(),
     recipientViewModel: RecipientViewModel = viewModel()
 ) {
-    val selected = remember { mutableStateOf(false) }
-
     AppScaffold(title = "Verify Deployment", navController = navController, bottomBar = {
         Column(
             modifier = Modifier
@@ -47,12 +43,6 @@ fun ContentVerificationScreen(
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-//            OutlinedButton(
-//                modifier = Modifier.fillMaxWidth(),
-//                onClick = { navController.popBackStack() }
-//            ) {
-//                Text("Change Recipient")
-//            }
             Button(
                 modifier = Modifier.fillMaxWidth(),
                 enabled = recipientViewModel.selectedRecipient.value != null,
@@ -72,58 +62,9 @@ fun ContentVerificationScreen(
                 .verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.Start
         ) {
-//            Tab(selected.value, onClick = {
-//                Log.d(LOG_TAG, "Tab Clicked")
-//            }) {
-//                Column(
-//                    Modifier.padding(10.dp).height(50.dp).fillMaxWidth(),
-//                    verticalArrangement = Arrangement.SpaceBetween
-//                ) {
-//                    Box(
-//                        Modifier.size(10.dp)
-//                            .align(Alignment.CenterHorizontally)
-//                            .background(
-//                                color =
-//                                if (selected.value) MaterialTheme.colorScheme.primary
-//                                else MaterialTheme.colorScheme.background
-//                            )
-//                    )
-//                    Text(
-//                        text = "Current",
-//                        style = MaterialTheme.typography.bodyLarge,
-//                        modifier = Modifier.align(Alignment.CenterHorizontally)
-//                    )
-//                }
-//            }
-
-//            Tab(selected.value, onClick = {
-//                Log.d(LOG_TAG, "Tab Clicked")
-//            }) {
-//                Column(
-//                    Modifier.padding(10.dp).height(50.dp).fillMaxWidth(),
-//                    verticalArrangement = Arrangement.SpaceBetween
-//                ) {
-//                    Box(
-//                        Modifier.size(10.dp)
-//                            .align(Alignment.CenterHorizontally)
-//                            .background(
-//                                color =
-//                                if (selected.value) MaterialTheme.colorScheme.primary
-//                                else MaterialTheme.colorScheme.background
-//                            )
-//                    )
-//                    Text(
-//                        text = "Current",
-//                        style = MaterialTheme.typography.bodyLarge,
-//                        modifier = Modifier.align(Alignment.CenterHorizontally)
-//                    )
-//                }
-//            }
-
-//            Text("Verify the content deployment")
 
             Text(
-                text = "After Update",
+                text = "Current Recipient",
                 modifier = Modifier
                     .padding(16.dp),
                 textAlign = TextAlign.Left,
@@ -132,28 +73,10 @@ fun ContentVerificationScreen(
             Divider()
 
             ElevatedCard(
-//                elevation = CardDefaults.cardElevation(
-//                    defaultElevation = 6.dp
-//                ),
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(vertical = 5.dp)
-//                    .padding(5.dp)
-//                    .fillMaxHeight()
-//                    .size(width = Modifier.fillMaxWidth(), height = 100.dp)
             ) {
-
-//
-//                ListItem(
-//                    headlineContent = { Text("One line list item with 24x24 icon") },
-//                    leadingContent = {
-//                        Icon(
-//                            Icons.Filled.Favorite,
-//                            contentDescription = "Localized description",
-//                        )
-//                    }
-//                )
-//                Divider()
 
                 Text(
                     modifier = Modifier.padding(10.dp),
@@ -165,10 +88,6 @@ fun ContentVerificationScreen(
                         }
                         // TODO: change to get district
                         append(recipientViewModel.defaultRecipient.value?.district ?: "N/A")
-//                    append("\n")
-
-//                    append("\n")
-//                    append(viewModel.talkingBookDeviceInfo.value?.deploymentPropertiesString)
 
                         withStyle(
                             SpanStyle(fontWeight = FontWeight.Bold)
@@ -188,7 +107,7 @@ fun ContentVerificationScreen(
             }
 
             Text(
-                text = "Current",
+                text = "Next Recipient",
                 modifier = Modifier
                     .padding(16.dp)
                     .padding(top = 15.dp),
@@ -198,42 +117,35 @@ fun ContentVerificationScreen(
             Divider()
 
             ElevatedCard(
-//                elevation = CardDefaults.cardElevation(
-//                    defaultElevation = 6.dp
-//                ),
                 modifier = Modifier
                     .fillMaxWidth()
-//                    .size(height = 100.dp)
             ) {
-
                 Text(
                     modifier = Modifier.padding(10.dp),
                     text = buildAnnotatedString {
-                    withStyle(
-                        SpanStyle(fontWeight = FontWeight.Bold)
-                    ) {
-                        append("District: \t")
-                    }
-                    append(recipientViewModel.selectedRecipient.value?.district)
+                        withStyle(
+                            SpanStyle(fontWeight = FontWeight.Bold)
+                        ) {
+                            append("District: \t")
+                        }
+                        append(recipientViewModel.selectedRecipient.value?.district)
 
-                    withStyle(
-                        SpanStyle(fontWeight = FontWeight.Bold)
-                    ) {
-                        append("\nCommunity: \t")
-                    }
-                    append(recipientViewModel.selectedRecipient.value?.communityname)
+                        withStyle(
+                            SpanStyle(fontWeight = FontWeight.Bold)
+                        ) {
+                            append("\nCommunity: \t")
+                        }
+                        append(recipientViewModel.selectedRecipient.value?.communityname)
 
-                    withStyle(
-                        SpanStyle(fontWeight = FontWeight.Bold)
-                    ) {
-                        append("\nGroup: \t")
-                    }
-                    append(recipientViewModel.selectedRecipient.value?.groupname)
+                        withStyle(
+                            SpanStyle(fontWeight = FontWeight.Bold)
+                        ) {
+                            append("\nGroup: \t")
+                        }
+                        append(recipientViewModel.selectedRecipient.value?.groupname)
 
-                })
+                    })
             }
-
-
         }
     }
 }
