4255a2f8cd80d091896d8d406e86210f27094d0d
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2024-06-27 08:43:06 +0000

    refactor(android): use ListItem for content package items

--- TalkingBookApp/app/src/main/java/org/literacybridge/talkingbookapp/ui/talkingbook_update/ContentVariantScreen.kt
+++ TalkingBookApp/app/src/main/java/org/literacybridge/talkingbookapp/ui/talkingbook_update/ContentVariantScreen.kt
@@ -7,21 +7,27 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme.typography
-import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import org.literacybridge.core.spec.Recipient
@@ -41,6 +47,7 @@ fun ContentVariantScreen(
     val pubCsv = createPropertiesMap()
     val recipient = Recipient(pubCsv)
     val defaultRecipient = recipientViewModel.defaultRecipient
+    var checked by remember { mutableStateOf(true) }
 
     AppScaffold(title = "Available Content Packages", navController = navController, bottomBar = {
         Column(
@@ -64,6 +71,15 @@ fun ContentVariantScreen(
         }
 
     }) { contentPadding ->
+//        DraggableLazyList(
+//            items = listOf("En", "Frensh", "We Mode It"),
+//            builder = { it ->
+//                Text("Working $it")
+//            },
+//            contentPadding = contentPadding,
+//            onSwap = { i1, i2 -> }
+//        )
+
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -71,45 +87,85 @@ fun ContentVariantScreen(
                 .padding(Constants.SCREEN_MARGIN)
                 .verticalScroll(rememberScrollState())
         ) {
-            val (selectedOption, onOptionSelected) = remember { mutableStateOf(languages!![1] ) }
-
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-            ) {
-                languages?.forEach { lang ->
-                    Row(
-                        Modifier
-                            .fillMaxWidth()
-                            .height(56.dp)
-                            .selectable(
-                                selected = (lang == selectedOption),
-                                onClick = {
-                                    onOptionSelected(lang)
-                                }
-                            )
-                            .padding(horizontal = 16.dp),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        RadioButton(
-                            selected = (lang == selectedOption),
-                            onClick = { onOptionSelected(lang) },
+            ListItem(
+                headlineContent = { Text("English (eng)") },
+                trailingContent = {
+                    Row {
+                        IconButton(
+                            onClick = {
+                            },
+                            content = {
+                                Icon(
+                                    Icons.Filled.KeyboardArrowDown,
+                                    contentDescription = "Move package down"
+                                )
+                            }
                         )
-                        Text(
-                            text = lang,
-                            style = typography.bodyMedium.merge(),
-                            modifier = Modifier.padding(start = 16.dp)
+                        IconButton(
+                            onClick = {
+                            },
+                            content = {
+                                Icon(
+                                    Icons.Filled.KeyboardArrowUp,
+                                    contentDescription = "Move package up"
+                                )
+                            }
                         )
-                        optionSelected = selectedOption
                     }
+                },
+                leadingContent = {
+                    Checkbox(
+                        checked = checked,
+                        onCheckedChange = { checked = it }
+                    )
+
+//                    Icon(
+//                        Icons.Filled.Favorite,
+//                        contentDescription = "Localized description",
+//                    )
                 }
-            }
+            )
+            HorizontalDivider()
+
+//            val (selectedOption, onOptionSelected) = remember { mutableStateOf(languages!![1]) }
+//
+//            Column(
+//                modifier = Modifier.fillMaxSize(),
+//                verticalArrangement = Arrangement.Center,
+//            ) {
+//                languages?.forEach { lang ->
+//                    Row(
+//                        Modifier
+//                            .fillMaxWidth()
+//                            .height(56.dp)
+//                            .selectable(
+//                                selected = (lang == selectedOption),
+//                                onClick = {
+//                                    onOptionSelected(lang)
+//                                }
+//                            )
+//                            .padding(horizontal = 16.dp),
+//                        verticalAlignment = Alignment.CenterVertically
+//                    ) {
+//                        RadioButton(
+//                            selected = (lang == selectedOption),
+//                            onClick = { onOptionSelected(lang) },
+//                        )
+//                        Text(
+//                            text = lang,
+//                            style = typography.bodyMedium.merge(),
+//                            modifier = Modifier.padding(start = 16.dp)
+//                        )
+//                        optionSelected = selectedOption
+//                    }
+//                }
+//            }
         }
 
     }
 }
 
-fun createPropertiesMap() : Map<String, String> {
+fun createPropertiesMap(): Map<String, String> {
     val pubCsv = hashMapOf<String, String>()
     pubCsv["recipientid"] = "12"
     pubCsv["district"] = "Default District"
