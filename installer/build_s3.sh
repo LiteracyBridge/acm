db0751bd8aed56492ed7557858173ceff7be7111
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2024-10-10 09:27:50 +0000

    ci(installer): increase timeout for s3 commands

--- installer/build_s3.sh
+++ installer/build_s3.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-aws s3 sync --delete ./ACM/ s3://amplio-software-update/desktop/ACM/
+aws s3 sync --cli-read-timeout 0 --cli-connect-timeout 0 --delete ./ACM/ s3://amplio-software-update/desktop/ACM/
 
-aws s3 cp  s3://amplio-software-update/desktop/ACM/build.properties s3://amplio-software-update/desktop/build.properties
-aws s3 cp  s3://amplio-software-update/desktop/ACM/update.bat       s3://amplio-software-update/desktop/update.bat
+aws s3 cp --cli-read-timeout 0 --cli-connect-timeout 0  s3://amplio-software-update/desktop/ACM/build.properties s3://amplio-software-update/desktop/build.properties
+aws s3 cp --cli-read-timeout 0 --cli-connect-timeout 0 s3://amplio-software-update/desktop/ACM/update.bat       s3://amplio-software-update/desktop/update.bat
 
 # Don't copy the installer to amplio-software-update; people won't run the installer from there.
 # aws s3 sync --exclude '*' --include AmplioSetup.exe ./Output/ s3://amplio-software-update/desktop/
 
 # Put a copy in downloads.amplio.org/software, for convenience
-aws s3 sync --exclude '*' --include AmplioSetup.exe ./Output/ s3://downloads.amplio.org/software/
+aws s3 sync --cli-read-timeout 0 --cli-connect-timeout 0 --exclude '*' --include AmplioSetup.exe ./Output/ s3://downloads.amplio.org/software/
