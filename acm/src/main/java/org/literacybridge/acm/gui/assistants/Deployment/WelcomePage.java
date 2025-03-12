b6fa7cd10c45d7ee34acc326f612a59a860bf81b
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2025-03-12 20:41:15 +0000

    feat(acm: deployment): add platform selection to deployment welcome page

--- acm/src/main/java/org/literacybridge/acm/gui/assistants/Deployment/WelcomePage.java
+++ acm/src/main/java/org/literacybridge/acm/gui/assistants/Deployment/WelcomePage.java
@@ -1,31 +1,25 @@
 package org.literacybridge.acm.gui.assistants.Deployment;
 
-import javafx.application.Platform;
-import javafx.embed.swing.JFXPanel;
-import javafx.scene.Scene;
-import javafx.scene.control.ComboBox;
-import javafx.scene.layout.StackPane;
-import org.literacybridge.acm.config.ACMConfiguration;
+
 import org.literacybridge.acm.gui.Assistant.Assistant.PageHelper;
 import org.literacybridge.acm.gui.Assistant.AssistantPage;
-import org.literacybridge.core.spec.ProgramSpec;
+import org.literacybridge.acm.store.DeplomentPlatform;
 
 import javax.swing.*;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
-import java.io.File;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class WelcomePage extends AssistantPage<DeploymentContext> {
 
     private final JComboBox<String> deploymentChooser;
+    private final JComboBox<String> platformChooser;
 
     private DeploymentContext context;
 
-    /// JFXPanel allows embedding JavaFX components inside a Swing-based application.
-    private final JFXPanel jfxPanel;
 
     WelcomePage(PageHelper<DeploymentContext> listener) {
         super(listener);
@@ -35,16 +29,16 @@ public class WelcomePage extends AssistantPage<DeploymentContext> {
         GridBagConstraints gbc = getGBC();
 
         JLabel welcome = new JLabel("<html>"
-            + "<span style='font-size:2.5em'>Welcome to the Deployment Assistant.</span>"
-            + "<br/><br/><p>The Assistant will guide you through creating a deployment. Here are the steps:</p>"
-            + "<ol>" + "<li> Choose the deployment number you want to create.</li>"
-            + "<li> The Assistant will see if the required files are available, based on information from the Program Specification. "
-            + "If files are missing, you may receive a warning message.</li>"
-            + "<li> If needed, you can then make minor changes to the deployment playlists.</li>"
-            + "<li> Once you are happy with the deployment, you will give your approval and the deployment will be created and published.</li>"
-            + "</ol>" + "<br/>Choose the deployment, and then click \"Next\" to get started. "
+                + "<span style='font-size:2.5em'>Welcome to the Deployment Assistant.</span>"
+                + "<br/><br/><p>The Assistant will guide you through creating a deployment. Here are the steps:</p>"
+                + "<ol>" + "<li> Choose the deployment number you want to create.</li>"
+                + "<li> The Assistant will see if the required files are available, based on information from the Program Specification. "
+                + "If files are missing, you may receive a warning message.</li>"
+                + "<li> If needed, you can then make minor changes to the deployment playlists.</li>"
+                + "<li> Once you are happy with the deployment, you will give your approval and the deployment will be created and published.</li>"
+                + "</ol>" + "<br/>Choose the deployment, and then click \"Next\" to get started. "
 
-            + "</html>");
+                + "</html>");
         add(welcome, gbc);
 
         Box hbox = Box.createHorizontalBox();
@@ -54,13 +48,20 @@ public class WelcomePage extends AssistantPage<DeploymentContext> {
         setComboWidth(deploymentChooser, "Choose...");
         deploymentChooser.setMaximumSize(deploymentChooser.getPreferredSize());
         hbox.add(deploymentChooser);
+        hbox.add(Box.createHorizontalStrut(20));
 
 
         /// Initialize JFXPanel to embed JavaFX dropdown in Swing.
-        jfxPanel = new JFXPanel();
-        initializeJavaFXDropdown();
-
-        hbox.add(jfxPanel);
+        hbox.add(new JLabel("and the platform: "));
+        platformChooser = new JComboBox<>();
+        platformChooser.addActionListener(this::onSelection);
+        setComboWidth(platformChooser, "Choose...");
+        platformChooser.setMaximumSize(platformChooser.getPreferredSize());
+        hbox.add(platformChooser);
+//        jfxPanel = new JFXPanel();
+//        initializeJavaFXDropdown();
+//
+//        hbox.add(jfxPanel);
 
         hbox.add(Box.createHorizontalGlue());
         add(hbox, gbc);
@@ -71,26 +72,6 @@ public class WelcomePage extends AssistantPage<DeploymentContext> {
         getProgramInformation();
     }
 
-   /// Sets up and embeds a JavaFX ComboBox inside the Swing JFXPanel.
-    private void initializeJavaFXDropdown() {
-        Platform.runLater(() -> {
-            ComboBox<String> fxComboBox = new ComboBox<>();
-            fxComboBox.getItems().addAll("Companion App", "Talking book");
-            fxComboBox.setPromptText("Choose from");
-
-            fxComboBox.getSelectionModel().select("Talking book");
-
-            fxComboBox.setOnAction(event -> {
-             String selectedValue =  fxComboBox.getSelectionModel().getSelectedItem();
-                System.out.println("Selected value: " + selectedValue);
-            });
-
-            // Set the scene with the JavaFX dropdown
-            StackPane root = new StackPane(fxComboBox);
-            Scene scene = new Scene(root);
-            jfxPanel.setScene(scene);
-        });
-    }
     /**
      * Called when a selection changes. Inspect the deployment and language
      * selections, and if both have a selection, enable the "Next" button.
@@ -106,16 +87,23 @@ public class WelcomePage extends AssistantPage<DeploymentContext> {
 
     @Override
     protected void onPageEntered(boolean progressing) {
-        // Fill deployments
+        // Fill platforms dropdown
+        platformChooser.removeAllItems();
+        platformChooser.addItem("Choose...");
+        platformChooser.addItem("Talking book");
+        platformChooser.addItem("Companion App");
+        platformChooser.setSelectedIndex(1);
 
+
+        // Fill deployments
         deploymentChooser.removeAllItems();
         deploymentChooser.addItem("Choose...");
         List<String> deployments = context.getProgramSpec().getDeployments()
-            .stream()
-            .map(d -> Integer.toString(d.deploymentnumber))
-            .collect(Collectors.toList());
+                .stream()
+                .map(d -> Integer.toString(d.deploymentnumber))
+                .collect(Collectors.toList());
         deployments
-            .forEach(deploymentChooser::addItem);
+                .forEach(deploymentChooser::addItem);
 
         // If only one deployment, or previously selected, auto-select.
         if (deployments.size() == 1) {
@@ -130,6 +118,14 @@ public class WelcomePage extends AssistantPage<DeploymentContext> {
     protected void onPageLeaving(boolean progressing) {
         // Since this is the welcome page, there must be something selected in order to move on.
         context.deploymentNo = getSelectedDeployment();
+
+        // Add platform to context
+        String val = (String) platformChooser.getSelectedItem();
+        if (Objects.equals(val, "Companion App")) {
+            context.platform = DeplomentPlatform.CompanionApp;
+        } else {
+            context.platform = DeplomentPlatform.TalkingBook;
+        }
     }
 
     @Override
