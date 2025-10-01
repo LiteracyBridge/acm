9a50f924baa543649c326ba209cdc6b1b4a9b293
Lawrence Ephrim <ephrimlawrence0@gmail.com>
2025-10-01 00:21:17 +0000

    fix: hard code sql migrations as data class objects instead of reading from file

---
+++ acm/src/main/java/org/literacybridge/acm/config/DatabaseMigrations.kt
@@ -0,0 +1,96 @@
+package org.literacybridge.acm.config
+
+data class Migration(val name: String, val sql: String)
+
+class DatabaseMigrations {
+    val version1: Migration = Migration("1-initial-migration", """" 
+        CREATE TABLE "migrations"
+    (
+        "id"        INTEGER NOT NULL UNIQUE,
+        "timestamp" TEXT    NOT NULL,
+        "name"      INTEGER NOT NULL UNIQUE,
+        PRIMARY KEY ("id" AUTOINCREMENT)
+    );
+    
+    CREATE TABLE "deployments"
+    (
+        "id"                INTEGER NOT NULL UNIQUE,
+        "name"              TEXT    NOT NULL UNIQUE,
+        "deployment_number" INTEGER NOT NULL,
+        "committed"         INTEGER NOT NULL DEFAULT 0,
+        PRIMARY KEY ("id" AUTOINCREMENT),
+        UNIQUE ("name", "deployment_number")
+    );
+    
+    CREATE TABLE "playlists"
+    (
+        "id"            INTEGER NOT NULL UNIQUE,
+        "title"         TEXT    NOT NULL UNIQUE,
+        "deployment_id" INTEGER,
+        "committed"     INTEGER NOT NULL DEFAULT 0,
+        PRIMARY KEY ("id" AUTOINCREMENT),
+        UNIQUE ("title", "deployment_id"),
+        FOREIGN KEY ("deployment_id") REFERENCES "deployments" ("id") ON DELETE CASCADE
+    );
+    
+    CREATE TABLE "audio_items"
+    (
+        "id"                    INTEGER NOT NULL UNIQUE,    -- random unique uuid
+        "title"                 TEXT    NOT NULL,
+        "playlist_id"           INTEGER,
+        "language"              TEXT    NOT NULL,
+        "duration"              TEXT,
+        "file_path"             TEXT,
+        "position"              INTEGER,
+        "format"                TEXT,
+        "default_category_code" TEXT,
+        "variant"               TEXT,
+        "sdg_goal_id"           INTEGER,
+        "sdg_goals"             TEXT,
+        "goal"                  TEXT,
+        "sdg_targets"           TEXT,
+        "key_points"            TEXT,
+        "sdg_target_id"         TEXT,
+        "category"              TEXT,
+        "status"                TEXT,
+        "volume"                INTEGER          DEFAULT 100,
+        "keywords"              TEXT,
+        "timing"                TEXT,
+        "speaker"       TEXT,
+        "device_id"             TEXT,
+        "acm_id"                TEXT,
+        "related_id"            TEXT,
+        "transcription"         TEXT,
+        "notes"                  TEXT,
+        "beneficiary"           TEXT,
+        "committed"             INTEGER NOT NULL DEFAULT 0, -- 0 = not committed, 1 = committed
+        "type"                  TEXT,                       -- "SystemPrompt" or "Message" or "PlaylistPrompt"
+        "source"                TEXT,
+        "audience"              TEXT,
+        "publisher"             TEXT,
+        "dtb_revision"          TEXT,
+        "recorded_at"           TEXT,
+        "created_at"            TEXT,
+        "deleted_at"            TEXT             DEFAULT NULL,
+        "updated_at"            TEXT             DEFAULT NULL,
+        PRIMARY KEY ("id" AUTOINCREMENT),
+        UNIQUE ("playlist_id", "title", "language"),
+        FOREIGN KEY ("playlist_id") REFERENCES "playlists" ("id") ON DELETE CASCADE
+    --     FOREIGN KEY ("related_id") REFERENCES "audio_items" ("acm_id") ON DELETE SET NULL
+    );
+    
+    CREATE TABLE "deployment_packages"
+    (
+        "id"            INTEGER NOT NULL UNIQUE,
+        "revision"      TEXT    NOT NULL,
+        "platform"      TEXT    NOT NULL DEFAULT 'TalkingBook', -- TalkingBook or CompanionApp
+        "created_at"    TEXT    NOT NULL,
+        "published"     INTEGER NOT NULL,                       -- 0 = not published, 1 = published
+        "deployment_id" INTEGER NOT NULL,
+        "metadata"      TEXT,
+        PRIMARY KEY ("id" AUTOINCREMENT),
+        FOREIGN KEY ("deployment_id") REFERENCES "deployments" ("id") ON DELETE CASCADE
+    );
+    """.trimMargin())
+
+}
